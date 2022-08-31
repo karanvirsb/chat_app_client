2909ce5f1b694b70a7dfcc8f5795eb9839d61c0b
mrboparai <karanvirboparai123@gmail.com>
2022-08-31 11:12:44 -0400

    created template for chat

--- src/Pages/Home/Components/Chat.tsx
+++ src/Pages/Home/Components/Chat.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Chat = () => {
-    return <div>Chat</div>;
+    return (
+        <div className='flex flex-col'>
+            <div className='outline outline-black flex-grow my-4'></div>
+            <div>
+                <input
+                    type='text'
+                    placeholder='Send a message'
+                    className='input w-full max-w-xs'
+                />
+                <button className='btn'>Send</button>
+            </div>
+        </div>
+    );
 };
 
 export default Chat;
