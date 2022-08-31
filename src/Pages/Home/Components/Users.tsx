9a0566a5703649ff81282589649746c8fb453ca1
mrboparai <karanvirboparai123@gmail.com>
2022-08-31 11:13:49 -0400

    created template for users

--- src/Pages/Home/Components/Users.tsx
+++ src/Pages/Home/Components/Users.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Users = () => {
-    return <div>Users</div>;
+    return (
+        <div>
+            <div className='collapse'>
+                <input type='checkbox' />
+                <div className='collapse-title text-xl font-medium'>Online</div>
+                <div className='collapse-content'>
+                    <ul>
+                        <li>general</li>
+                    </ul>
+                </div>
+            </div>
+            <div className='collapse'>
+                <input type='checkbox' />
+                <div className='collapse-title text-xl font-medium'>
+                    Offline
+                </div>
+                <div className='collapse-content'>
+                    <ul>
+                        <li>general</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Users;
