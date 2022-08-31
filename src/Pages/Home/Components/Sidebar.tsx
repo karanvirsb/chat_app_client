b1d89d5038d1a49ec95c13d9fb3ce3cc8572fe3e
mrboparai <karanvirboparai123@gmail.com>
2022-08-31 11:03:43 -0400

    added template

--- src/Pages/Home/Components/Sidebar.tsx
+++ src/Pages/Home/Components/Sidebar.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Sidebar = () => {
-    return <div>Sidebar</div>;
+    return (
+        <div className='grid grid-cols-[1fr_2fr]'>
+            {/*  for groups*/}
+            <div>
+                <div>Logo</div>
+                <div className='divider'></div>
+            </div>
+            {/* for chat */}
+            <div>
+                <div>Group Name</div>
+                <div className='collapse'>
+                    <input type='checkbox' />
+                    <div className='collapse-title text-xl font-medium'>
+                        Text Channels
+                    </div>
+                    <div className='collapse-content'>
+                        <ul>
+                            <li>general</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Sidebar;
