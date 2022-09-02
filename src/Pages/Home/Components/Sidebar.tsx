3aefba377dcb4ddd95b5fb33da8ae9120d008319
mrboparai <karanvirboparai123@gmail.com>
2022-09-02 10:33:22 -0400

    added btn for adding groups

--- src/Pages/Home/Components/Sidebar.tsx
+++ src/Pages/Home/Components/Sidebar.tsx
@@ -10,6 +10,25 @@ const Sidebar = () => {
                     <img src={Logo} className='min-w-[50px] max-w-[75px]'></img>
                 </div>
                 <div className='divider'></div>
+                <div>
+                    <ul></ul>
+                    <button className='btn btn-circle'>
+                        <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            fill='none'
+                            viewBox='0 0 24 24'
+                            strokeWidth={1.5}
+                            stroke='currentColor'
+                            className='w-6 h-6'
+                        >
+                            <path
+                                strokeLinecap='round'
+                                strokeLinejoin='round'
+                                d='M12 4.5v15m7.5-7.5h-15'
+                            />
+                        </svg>
+                    </button>
+                </div>
             </div>
             {/* for chat */}
             <div>
