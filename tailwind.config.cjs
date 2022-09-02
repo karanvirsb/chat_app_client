6a224af83bdb3cbdea55492e3c819cc410658071
mrboparai <karanvirboparai123@gmail.com>
2022-09-02 11:30:47 -0400

    added a color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,9 @@ module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {},
+        colors: {
+            groupBar: "#242933",
+        },
     },
     plugins: [require("daisyui")],
 };
