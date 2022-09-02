f54e040df7ba862e34d22061f12bf781d37b5bb8
mrboparai <karanvirboparai123@gmail.com>
2022-09-02 11:02:56 -0400

    added png and removed extra divs

--- src/Pages/Home/Components/Sidebar.tsx
+++ src/Pages/Home/Components/Sidebar.tsx
@@ -1,34 +1,32 @@
 import React from "react";
-import Logo from "../../../assets/logo-nobg.svg";
+import Logo from "../../../assets/logo-nobg.png";
 
 const Sidebar = () => {
     return (
         <div className='grid grid-cols-[1fr_3fr]'>
             {/*  for groups*/}
-            <div className=''>
-                <div className='flex justify-center items-center'>
-                    <img src={Logo} className='min-w-[50px] max-w-[75px]'></img>
-                </div>
+            <div className='flex flex-col items-center'>
+                <button className='btn btn-circle'>
+                    <img src={Logo} className='rounded-full'></img>
+                </button>
                 <div className='divider'></div>
-                <div>
-                    <ul></ul>
-                    <button className='btn btn-circle'>
-                        <svg
-                            xmlns='http://www.w3.org/2000/svg'
-                            fill='none'
-                            viewBox='0 0 24 24'
-                            strokeWidth={1.5}
-                            stroke='currentColor'
-                            className='w-6 h-6'
-                        >
-                            <path
-                                strokeLinecap='round'
-                                strokeLinejoin='round'
-                                d='M12 4.5v15m7.5-7.5h-15'
-                            />
-                        </svg>
-                    </button>
-                </div>
+                <ul></ul>
+                <button className='btn btn-circle'>
+                    <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        strokeWidth={1.5}
+                        stroke='currentColor'
+                        className='w-6 h-6'
+                    >
+                        <path
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
+                            d='M12 4.5v15m7.5-7.5h-15'
+                        />
+                    </svg>
+                </button>
             </div>
             {/* for chat */}
             <div>
