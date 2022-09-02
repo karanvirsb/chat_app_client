d15c25b2fcc827a25bfa3c262e60d3f889235909
mrboparai <karanvirboparai123@gmail.com>
2022-09-02 12:02:41 -0400

    added bg and fixed input

--- src/Pages/Home/Components/Chat.tsx
+++ src/Pages/Home/Components/Chat.tsx
@@ -2,15 +2,16 @@ import React from "react";
 
 const Chat = () => {
     return (
-        <div className='flex flex-col'>
-            <div className='outline outline-black flex-grow my-4'></div>
-            <div>
+        <div className='flex flex-col py-4 bg-chat-bg'>
+            <div className='w-full bg-chat-bg'>General</div>
+            {/* Create toggle for users */}
+            <div className='flex-grow w-full'></div>
+            <div className='input-group'>
                 <input
                     type='text'
                     placeholder='Send a message'
-                    className='input w-full max-w-xs'
+                    className='input input-bordered w-full p-4 focus:outline-none'
                 />
-                <button className='btn'>Send</button>
             </div>
         </div>
     );
