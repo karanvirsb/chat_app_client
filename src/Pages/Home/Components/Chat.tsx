7927f3800fb48d42f55272b7e07640a3f22dbcdf
mrboparai <karanvirboparai123@gmail.com>
2022-09-02 12:08:40 -0400

    added users

--- src/Pages/Home/Components/Chat.tsx
+++ src/Pages/Home/Components/Chat.tsx
@@ -1,18 +1,26 @@
 import React from "react";
+import Users from "./Users";
 
 const Chat = () => {
     return (
         <div className='flex flex-col py-4 bg-chat-bg'>
-            <div className='w-full bg-chat-bg'>General</div>
-            {/* Create toggle for users */}
-            <div className='flex-grow w-full'></div>
-            <div className='input-group'>
-                <input
-                    type='text'
-                    placeholder='Send a message'
-                    className='input input-bordered w-full p-4 focus:outline-none'
-                />
+            <div className='w-full btn bg-chat-bg'>General</div>
+            <div className='grid grid-cols-[2fr_1fr] w-full h-full'>
+                <div className='flex flex-col h-full'>
+                    <div className='flex-grow w-full p-4'>
+                        {/* Create chat component */}
+                    </div>
+                    <div className='input-group p-4'>
+                        <input
+                            type='text'
+                            placeholder='Send a message'
+                            className='input input-bordered w-full focus:outline-none'
+                        />
+                    </div>
+                </div>
+                <Users></Users>
             </div>
+            {/* Create toggle for users */}
         </div>
     );
 };
