f7f83b30d99f1918bd69d078739082d8eb1ff2d4
mrboparai <karanvirboparai123@gmail.com>
2022-09-03 12:24:47 -0400

    created a collapse component

---
+++ src/Components/Collapse/Collapse.tsx
@@ -0,0 +1,39 @@
+import React, { MouseEventHandler, useState } from "react";
+
+type props = {
+    children: any;
+    title: string;
+    clickEvent?: MouseEventHandler<HTMLElement>;
+};
+
+const Collapse = ({ children, title, clickEvent }: props) => {
+    const [clicked, setClicked] = useState(false);
+    return (
+        <>
+            <div className='flex items-center px-4'>
+                <div
+                    className='collapse-title text-xl font-medium flex gap-4'
+                    onClick={() => setClicked((prev) => !prev)}
+                >
+                    {clicked ? (
+                        <div className='-rotate-90'>&#10094;</div>
+                    ) : (
+                        <div className=''>&#10095;</div>
+                    )}
+                    <span>{title}</span>
+                </div>
+                {clickEvent && (
+                    <span
+                        className='cursor-pointer text-xl'
+                        onClick={clickEvent}
+                    >
+                        +
+                    </span>
+                )}
+            </div>
+            {clicked && children}
+        </>
+    );
+};
+
+export default Collapse;
