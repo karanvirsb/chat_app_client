6da34de2fd590cd1e7005568dafd12993b90f7b4
mrboparai <karanvirboparai123@gmail.com>
2022-10-20 12:04:19 -0400

    added todo

--- src/Pages/Home/Components/Users.tsx
+++ src/Pages/Home/Components/Users.tsx
@@ -3,6 +3,8 @@ import React from "react";
 const Users = () => {
     return (
         <div className='bg-groupInfo-bg px-4 text-white'>
+            {/* TODO add top bar with search */}
+            {/* TODO create tabs for online and offline users */}
             <div className='collapse collapse-open'>
                 <input type='checkbox' />
                 <div className='collapse-title text-xl font-medium'>Online</div>
