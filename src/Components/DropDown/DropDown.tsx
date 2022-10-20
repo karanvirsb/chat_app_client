e7eba2a6a6848d90c06bfb2583c9aa64c0b08a6d
mrboparai <karanvirboparai123@gmail.com>
2022-10-20 12:01:07 -0400

    created drop down component

---
+++ src/Components/DropDown/DropDown.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const DropDown = () => {
+    // TODO add options (right, top, bottom, left) also collapse symbol + children
+    return <div>DropDown</div>;
+};
+
+export default DropDown;
