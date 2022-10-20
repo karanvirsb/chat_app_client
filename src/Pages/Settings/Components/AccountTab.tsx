4c00b1697df94c3a9e5aad43ac693ce34b649d8e
mrboparai <karanvirboparai123@gmail.com>
2022-10-20 12:07:58 -0400

    created an account tab

---
+++ src/Pages/Settings/Components/AccountTab.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// TODO add username, email, password info and edit btn
+// TODO add delete account button
+const AccountTab = () => {
+    return <div>AccountTab</div>;
+};
+
+export default AccountTab;
