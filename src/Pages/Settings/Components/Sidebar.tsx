7a37d4ce96b1ce9e3a6d07a822869e56e43878bd
mrboparai <karanvirboparai123@gmail.com>
2022-10-20 12:08:04 -0400

    created a sidebar

---
+++ src/Pages/Settings/Components/Sidebar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// TODO Add account settings and back button
+// TODO add tab display
+const Sidebar = () => {
+    return <div>Sidebar</div>;
+};
+
+export default Sidebar;
