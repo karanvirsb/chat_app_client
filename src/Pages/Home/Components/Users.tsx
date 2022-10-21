9a4fe19b52fe3020552a28e640a374addb16f0e1
mrboparai <karanvirboparai123@gmail.com>
2022-10-21 10:12:40 -0400

    added drop shadow and padding y

--- src/Pages/Home/Components/Users.tsx
+++ src/Pages/Home/Components/Users.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Users = () => {
     return (
         <div className='flex flex-col'>
-            <div className='bg-chat-bg flex items-center font-semibold drop-shadow-md px-4 w-full h-16 text-white'></div>
+            <div className='bg-chat-bg drop-shadow-md flex items-center font-semibold py-2 px-4 w-full h-16 text-white'></div>
             <div className='bg-groupInfo-bg px-4 text-white flex-grow'>
                 {/* TODO add top bar with search */}
                 {/* TODO create tabs for online and offline users */}
