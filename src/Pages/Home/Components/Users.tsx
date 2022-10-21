875954b113a94450d00438ab53cb5eb2275fc31b
mrboparai <karanvirboparai123@gmail.com>
2022-10-21 10:04:37 -0400

    craeted a nav bar at the top

--- src/Pages/Home/Components/Users.tsx
+++ src/Pages/Home/Components/Users.tsx
@@ -2,27 +2,32 @@ import React from "react";
 
 const Users = () => {
     return (
-        <div className='bg-groupInfo-bg px-4 text-white'>
-            {/* TODO add top bar with search */}
-            {/* TODO create tabs for online and offline users */}
-            <div className='collapse collapse-open'>
-                <input type='checkbox' />
-                <div className='collapse-title text-xl font-medium'>Online</div>
-                <div className='collapse-content'>
-                    <ul>
-                        <li>general</li>
-                    </ul>
+        <div className='flex flex-col'>
+            <div className='bg-chat-bg flex items-center font-semibold drop-shadow-md px-4 w-full h-16 text-white'></div>
+            <div className='bg-groupInfo-bg px-4 text-white flex-grow'>
+                {/* TODO add top bar with search */}
+                {/* TODO create tabs for online and offline users */}
+                <div className='collapse collapse-open'>
+                    <input type='checkbox' />
+                    <div className='collapse-title text-xl font-medium'>
+                        Online
+                    </div>
+                    <div className='collapse-content'>
+                        <ul>
+                            <li>general</li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-            <div className='collapse collapse-open'>
-                <input type='checkbox' />
-                <div className='collapse-title text-xl font-medium'>
-                    Offline
-                </div>
-                <div className='collapse-content'>
-                    <ul>
-                        <li>general</li>
-                    </ul>
+                <div className='collapse collapse-open'>
+                    <input type='checkbox' />
+                    <div className='collapse-title text-xl font-medium'>
+                        Offline
+                    </div>
+                    <div className='collapse-content'>
+                        <ul>
+                            <li>general</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
