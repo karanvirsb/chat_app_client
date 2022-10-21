b9b14eaacd79b296edf1c35b08e07b4de1d0d653
mrboparai <karanvirboparai123@gmail.com>
2022-10-21 10:03:53 -0400

    move bar up to the top

--- src/Pages/Home/Components/Chat.tsx
+++ src/Pages/Home/Components/Chat.tsx
@@ -3,26 +3,28 @@ import Users from "./Users";
 
 const Chat = () => {
     return (
-        <div className='bg-chat-bg flex flex-col'>
+        <div className='flex flex-col'>
             <div className='bg-chat-bg border-b border-groupBar-bg flex items-center font-semibold drop-shadow-md py-2 px-4 w-full h-16 text-white'>
                 General
             </div>
-            <div className='grid grid-cols-[3fr_1fr] w-full h-full'>
-                <div className='flex flex-col h-full'>
-                    <div className='flex-grow p-4 w-full '>
-                        {/* TODO Create chat component */}
+            <div className='bg-chat-bg flex-grow'>
+                <div className='grid grid-cols-[3fr_1fr] w-full h-full'>
+                    <div className='flex flex-col h-full'>
+                        <div className='flex-grow p-4 w-full '>
+                            {/* TODO Create chat component */}
+                        </div>
+                        <div className='input-group p-4'>
+                            <input
+                                type='text'
+                                placeholder='Send a message'
+                                className='input input-bordered bg-[#2a303c] w-full focus:outline-none'
+                            />
+                        </div>
                     </div>
-                    <div className='input-group p-4'>
-                        <input
-                            type='text'
-                            placeholder='Send a message'
-                            className='input input-bordered bg-[#2a303c] w-full focus:outline-none'
-                        />
-                    </div>
+                    {/* <Users></Users> */}
                 </div>
-                <Users></Users>
+                {/* TODO Create toggle for users */}
             </div>
-            {/* TODO Create toggle for users */}
         </div>
     );
 };
