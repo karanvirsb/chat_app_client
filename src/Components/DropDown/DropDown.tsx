7b7040b672e1d1130c16e8a093e8717023fc9152
mrboparai <karanvirboparai123@gmail.com>
2022-10-21 10:25:51 -0400

    created a dropdown component

--- src/Components/DropDown/DropDown.tsx
+++ src/Components/DropDown/DropDown.tsx
@@ -1,8 +1,44 @@
 import React from "react";
 
-const DropDown = () => {
-    // TODO add options (right, top, bottom, left) also collapse symbol + children
-    return <div>DropDown</div>;
+type props = {
+    direction: "dropdown-top" | "dropdown-left" | "dropdown-right";
+    align?: "dropdown-end";
+    hover?: boolean;
+    btnClass?: string;
+    listClass?: string;
+    btnChildren: JSX.Element;
+    listChildren: JSX.Element;
+};
+
+const DropDown = ({
+    direction,
+    align,
+    hover,
+    btnClass,
+    listClass,
+    btnChildren,
+    listChildren,
+}: props) => {
+    return (
+        <div
+            className={`dropdown ${direction} ${align} ${
+                hover && "dropdown-hover"
+            } mt-auto`}
+        >
+            <label
+                tabIndex={0}
+                className={`${btnClass ? btnClass : "btn btn-circle m-1"}`}
+            >
+                {btnChildren}
+            </label>
+            <ul
+                tabIndex={1}
+                className={`dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52 ${listClass}`}
+            >
+                {listChildren}
+            </ul>
+        </div>
+    );
 };
 
 export default DropDown;
