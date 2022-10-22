b6f5ede2c405dff4d60eed15298d0bd8e45f50fb
mrboparai <karanvirboparai123@gmail.com>
2022-10-22 10:47:16 -0400

    created a modal slice

---
+++ src/Redux/slices/modalSlice.ts
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+// import type { RootState } from "../store";
+
+// Define a type for the slice state
+interface ModalState {
+    open: boolean;
+    options: any;
+    modalName: "changeGroupName" | "";
+}
+
+// Define the initial state using that type
+const initialState: ModalState = {
+    modalName: "",
+    options: {},
+    open: false,
+};
+
+export const modalSlice = createSlice({
+    name: "counter",
+    // `createSlice` will infer the state type from the `initialState` argument
+    initialState,
+    reducers: {
+        setModal: (state, action: PayloadAction<ModalState>) => {
+            state = action.payload;
+        },
+    },
+});
+
+export const { setModal } = modalSlice.actions;
+
+// Other code such as selectors can use the imported `RootState` type
+// export const selectModalState = (state: RootState) => state.counter.value;
+
+export default modalSlice.reducer;
