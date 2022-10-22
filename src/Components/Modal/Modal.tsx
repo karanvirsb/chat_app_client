4d845c90c9442b1a21c4e2b689a01d0c0b7aa4a7
mrboparai <karanvirboparai123@gmail.com>
2022-10-22 09:41:55 -0400

    added modal name

--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -4,12 +4,23 @@ type props = {
     children: JSX.Element;
     bgClass?: string;
     modalClass?: string;
+    modalName?: string;
 };
 // TODO create modal with children, name
-const Modal = ({ children, bgClass = "", modalClass = "" }: props) => {
+const Modal = ({
+    children,
+    bgClass = "",
+    modalClass = "",
+    modalName = "",
+}: props) => {
     return (
         <div className={`modal modal-bottom sm:modal-middle ${bgClass}`}>
-            <div className={`modal-box ${modalClass}`}>{children}</div>
+            <div className={`modal-box relative ${modalClass}`}>
+                <div className='absolute top-[2px] left-[10px]'>
+                    {modalName}
+                </div>
+                {children}
+            </div>
         </div>
     );
 };
