33b78a1514fb392366cc8f49803438dce9151193
mrboparai <karanvirboparai123@gmail.com>
2022-10-22 10:59:28 -0400

    added store and provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./Redux/store";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <Router>
-            <App />
+            <Provider store={store}>
+                <App />
+            </Provider>
         </Router>
     </React.StrictMode>
 );
