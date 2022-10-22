f7249f87a2289d4c3f33582259cfaae9db497a85
mrboparai <karanvirboparai123@gmail.com>
2022-10-22 09:06:38 -0400

    added svg for side open and close

--- src/Pages/Home/Components/Chat.tsx
+++ src/Pages/Home/Components/Chat.tsx
@@ -1,11 +1,56 @@
 import React from "react";
-import Users from "./Users";
 
-const Chat = () => {
+type props = {
+    isSideBarOpen: boolean;
+    setIsSideBarOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    isUserMenuOpen: boolean;
+    setIsUserMenuOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const Chat = ({
+    isSideBarOpen,
+    setIsSideBarOpen,
+    isUserMenuOpen,
+    setIsUserMenuOpen,
+}: props) => {
     return (
         <div className='flex flex-col'>
             <div className='bg-chat-bg border-b border-r-0 border-groupBar-bg flex items-center font-semibold drop-shadow-md py-2 px-4 w-full h-16 text-white'>
-                General
+                <button className='mr-2'>
+                    {isSideBarOpen ? (
+                        <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            fill='none'
+                            viewBox='0 0 24 24'
+                            strokeWidth={1.5}
+                            stroke='currentColor'
+                            className='w-6 h-6 hover:text-black'
+                        >
+                            <path
+                                strokeLinecap='round'
+                                strokeLinejoin='round'
+                                d='M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75'
+                            />
+                        </svg>
+                    ) : (
+                        <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            fill='none'
+                            viewBox='0 0 24 24'
+                            strokeWidth={1.5}
+                            stroke='currentColor'
+                            className='w-6 h-6 hover:text-black'
+                        >
+                            <path
+                                strokeLinecap='round'
+                                strokeLinejoin='round'
+                                d='M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9'
+                            />
+                        </svg>
+                    )}
+                </button>
+                {/*TODO Channel name goes here*/}
+                <span>General</span>
             </div>
             <div className='bg-chat-bg flex-grow'>
                 <div className='grid grid-cols-[3fr_1fr] w-full h-full'>
