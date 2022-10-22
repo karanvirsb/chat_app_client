0546257234949a54ce3fd3ed519c9178b2241da0
mrboparai <karanvirboparai123@gmail.com>
2022-10-22 09:38:54 -0400

    added modal class jsx and props

--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 
+type props = {
+    children: JSX.Element;
+    bgClass?: string;
+    modalClass?: string;
+};
 // TODO create modal with children, name
-const Modal = () => {
-    return <div>Modal</div>;
+const Modal = ({ children, bgClass = "", modalClass = "" }: props) => {
+    return (
+        <div className={`modal modal-bottom sm:modal-middle ${bgClass}`}>
+            <div className={`modal-box ${modalClass}`}>{children}</div>
+        </div>
+    );
 };
 
 export default Modal;
