6d3b3a401fa9aeb480349d7111059eb8812bb4fd
mrboparai <karanvirboparai123@gmail.com>
2022-10-22 08:57:05 -0400

    removed sidebar info

--- src/Pages/Home/Components/Sidebar.tsx
+++ src/Pages/Home/Components/Sidebar.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Logo from "../../../assets/logo-nobg.png";
-import Collapse from "../../../Components/Collapse/Collapse";
 import { signOut } from "supertokens-auth-react/recipe/emailpassword";
 import DropDown from "../../../Components/DropDown/DropDown";
+import SidebarInfo from "./SidebarInfo";
 
 export default function Sidebar() {
     return (
-        <div className='grid grid-cols-[1fr_5fr]'>
+        // <div className='grid grid-cols-[1fr_5fr]'>
+        <>
             {/*  for groups*/}
             <div className='flex flex-col items-center bg-groupBar-bg py-4 px-2'>
                 <button className='btn btn-circle'>
@@ -75,49 +76,8 @@ export default function Sidebar() {
                 </DropDown>
             </div>
             {/* for chat */}
-            <div className='flex flex-col'>
-                <DropDown
-                    btnChildren='Group Name'
-                    btnClass='btn font-bold h-16 rounded-none w-full'
-                    dropDownClass='flex flex-col items-center'
-                    listClass='relative top-[125%] w-[90%]'
-                    symbol={true}
-                >
-                    <>
-                        <li>
-                            <button className='btn bg-btn-primary border-none font-bold mb-2 text-btn-mutations-text hover:bg-btn-primary-hover'>
-                                Change Group Name
-                            </button>
-                        </li>
-                        <li>
-                            <button className='btn bg-btn-primary border-none font-bold mb-2 text-btn-mutations-text hover:bg-btn-primary-hover'>
-                                Invite User
-                            </button>
-                        </li>
-                        <li>
-                            <button className='btn bg-btn-mutations border-none font-bold mb-2 text-btn-mutations-text hover:bg-btn-mutations-hover'>
-                                Delete Group
-                            </button>
-                        </li>
-                        <li>
-                            <button className='btn bg-btn-mutations border-none font-bold mb-2 text-btn-mutations-text hover:bg-btn-mutations-hover'>
-                                Leave Group
-                            </button>
-                        </li>
-                    </>
-                </DropDown>
-                {/* <div className='bg-groupInfo-bg border-b border-chat-bg flex items-center font-semibold drop-shadow-md py-2 px-4 w-full h-16 text-white'>
-                    Group Name
-                </div> */}
-                <div className='bg-groupInfo-bg flex-grow text-white'>
-                    <Collapse title='Text Channels'>
-                        <ul>
-                            <li>general</li>
-                        </ul>
-                    </Collapse>
-                </div>
-            </div>
-        </div>
+        </>
+        // </div>
     );
 }
 
