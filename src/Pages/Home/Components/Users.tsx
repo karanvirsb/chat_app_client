7e48edf8e32f690e5008b8820f22f456f3ab7c6f
mrboparai <karanvirboparai123@gmail.com>
2022-10-23 18:07:46 -0400

    removed top bar

--- src/Pages/Home/Components/Users.tsx
+++ src/Pages/Home/Components/Users.tsx
@@ -6,20 +6,18 @@ type props = {
 };
 // TODO add animation on close
 // Todo move into users
-export default function Users({ isUserMenuOpen }: props) {
+export default function Users() {
     return (
-        <div className='flex flex-col'>
-            <div className='bg-chat-bg drop-shadow-md flex items-center font-semibold py-2 px-4 w-full h-16 text-white'></div>
-            {isUserMenuOpen && (
-                <div className='bg-groupInfo-bg text-white flex-grow'>
-                    {/* TODO add top bar with search */}
-                    {/* TODO create tabs for online and offline users */}
-                    <Tabs
-                        components={createComponents()}
-                        tabs={createTabs()}
-                    ></Tabs>
-                </div>
-            )}
+        <div className='flex flex-col flex-grow max-w-[250px]'>
+            {/* <div className='bg-chat-bg drop-shadow-md flex items-center font-semibold py-2 px-4 w-full h-16 text-white'></div> */}
+            <div className='bg-groupInfo-bg text-white flex-grow'>
+                {/* TODO add top bar with search */}
+                {/* TODO create tabs for online and offline users */}
+                <Tabs
+                    components={createComponents()}
+                    tabs={createTabs()}
+                ></Tabs>
+            </div>
         </div>
     );
 
