76942441c3182f58d5241473e55f21d8f7e5a879
mrboparai <karanvirboparai123@gmail.com>
2022-10-23 08:00:47 -0400

    created state for new name

--- src/Components/Modal/ChangeGroupNameModal.tsx
+++ src/Components/Modal/ChangeGroupNameModal.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { ChangeEvent, ChangeEventHandler, useState } from "react";
+import { useAppDispatch } from "../../Hooks/reduxHooks";
+import { resetModal } from "../../Redux/slices/modalSlice";
 import Modal from "./Modal";
 
 // TODO give group id
 export default function ChangeGroupNameModal() {
+    const [newName, setNewName] = useState<string>("");
+    const dispatch = useAppDispatch();
+
     return (
         <Modal modalName='Change Group Name'>
             <>
@@ -12,7 +17,7 @@ export default function ChangeGroupNameModal() {
                     </label>
                     <input
                         type='text'
-                        placeholder='Type here'
+                        placeholder=''
                         className='input input-bordered w-full max-w-xs'
                         contentEditable={false}
                     />
@@ -23,15 +28,31 @@ export default function ChangeGroupNameModal() {
                     </label>
                     <input
                         type='text'
-                        placeholder='Type here'
+                        placeholder='New group name'
                         className='input input-bordered w-full max-w-xs'
+                        onChange={handleChange}
                     />
                 </div>
                 <div>
-                    <button className='btn'>Change</button>
-                    <button className='btn'>Cancel</button>
+                    <button className='btn' onClick={handleSubmit}>
+                        Change
+                    </button>
+                    <button className='btn' onClick={closeModal}>
+                        Cancel
+                    </button>
                 </div>
             </>
         </Modal>
     );
+
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setNewName(() => e.target.value);
+    }
+
+    function closeModal() {
+        dispatch(resetModal());
+    }
+
+    // api slice
+    function handleSubmit() {}
 }
