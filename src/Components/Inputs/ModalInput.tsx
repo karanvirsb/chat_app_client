107115c0f9506e2f07d0c5d710a5cd40aefcb32e
mrboparai <karanvirboparai123@gmail.com>
2022-10-23 10:22:09 -0400

    added error message and organized props in alphabetical order

--- src/Components/Inputs/ModalInput.tsx
+++ src/Components/Inputs/ModalInput.tsx
@@ -1,44 +1,52 @@
 import React from "react";
 
 type props = {
-    labelClass?: string;
-    inputClass?: string;
-    formClass?: string;
-    labelName: string;
-    inputId: string;
+    children?: JSX.Element;
     editable?: boolean;
+    errorMsg?: string;
+    formClass?: string;
+    inputClass?: string;
+    inputId: string;
+    labelClass?: string;
+    labelName: string;
+    onChange?: React.ChangeEventHandler<HTMLInputElement>;
     placeholder?: string;
     type?: React.HTMLInputTypeAttribute | undefined;
     value?: any;
-    onChange?: React.ChangeEventHandler<HTMLInputElement>;
-    children?: JSX.Element;
 };
 
 export default function ModalInput({
-    labelClass = "",
-    inputClass = "",
-    formClass = "",
-    labelName = "",
-    inputId = "",
+    children,
     editable = true,
+    errorMsg = "",
+    formClass = "",
+    inputClass = "",
+    inputId = "",
+    labelClass = "",
+    labelName = "",
+    onChange,
     placeholder = "Type Here",
     type = "",
     value,
-    onChange,
-    children,
 }: props) {
     return (
         <div className={`form-control w-full ${formClass}`}>
-            <label className={`label ${labelClass}`} htmlFor={inputId}>
+            <label
+                className={`label flex gap-4 ${labelClass}`}
+                htmlFor={inputId}
+            >
                 <span className={`label-text text-white ${labelClass}`}>
                     {labelName}
                 </span>
+                <span className='text-red-400'>{errorMsg}</span>
             </label>
             <input
                 id={inputId}
                 type={type}
                 placeholder={placeholder}
-                className={`input input-bordered bg-[#2A303C] w-full ${inputClass}`}
+                className={`input input-bordered bg-[#2A303C] w-full ${inputClass} ${
+                    errorMsg && "outline outline-2 outline-red-400"
+                }`}
                 contentEditable={editable}
                 onChange={onChange ?? undefined}
                 value={value}
