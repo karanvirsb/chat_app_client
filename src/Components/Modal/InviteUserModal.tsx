028c625bb9a9955c812e9a6d2529e40e8bdc74ac
mrboparai <karanvirboparai123@gmail.com>
2022-10-23 10:22:30 -0400

    added modal input for invite user

--- src/Components/Modal/InviteUserModal.tsx
+++ src/Components/Modal/InviteUserModal.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import BtnCallToAction from "../Buttons/BtnCallToAction";
 import ModalInput from "../Inputs/ModalInput";
 import Modal from "./Modal";
 
 export default function InviteUserModal() {
+    const [usernameSearch, setUsernameSearch] = useState("");
+
+    // TODO useEffect that checks user name in database
+
     return (
         <Modal modalName='Invite User'>
             <div className='flex flex-col gap-4 mt-6'>
@@ -20,6 +24,15 @@ export default function InviteUserModal() {
                         onClick={handleCopyInviteCode}
                     ></BtnCallToAction>
                 </ModalInput>
+                <ModalInput
+                    inputId='inviteUserSearch'
+                    labelName='Invite User'
+                    type='search'
+                    placeholder='Search User'
+                    value=''
+                    onChange={handleUserSearch}
+                    errorMsg=''
+                ></ModalInput>
             </div>
         </Modal>
     );
@@ -27,5 +40,9 @@ export default function InviteUserModal() {
     function handleCopyInviteCode() {
         const text = "copied code"; // TODO replace with invite code url
         navigator.clipboard.writeText(text);
+    }
+
+    function handleUserSearch(e: React.ChangeEvent<HTMLInputElement>) {
+        setUsernameSearch(() => e.target.value);
     }
 }
