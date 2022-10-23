4795241753bf99a90f57b5ed7cfae8a20ddf7ee8
mrboparai <karanvirboparai123@gmail.com>
2022-10-23 09:18:51 -0400

    changed cta

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -23,6 +23,8 @@ module.exports = {
                 "btn-mutations-hover": "hsla(360, 62%, 55%, 100%)",
                 "btn-primary": "hsla(219, 18%, 50%, 100%)",
                 "btn-primary-hover": "hsla(219, 18%, 35%, 100%)",
+                "btn-cta": "hsla(219, 18%, 45%, 100%)",
+                "btn-cta-hover": "hsla(219, 18%, 40%, 100%)",
             },
         },
     },
