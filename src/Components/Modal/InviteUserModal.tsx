69684addaa8e8c247c6fe929cec4dd1bd6dc91aa
mrboparai <karanvirboparai123@gmail.com>
2022-10-23 09:59:40 -0400

    created a template invite user modal

---
+++ src/Components/Modal/InviteUserModal.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Modal from "./Modal";
+
+export default function InviteUserModal() {
+    return (
+        <Modal modalName='Invite User'>
+            <div className='flex flex-col gap-4 mt-6'></div>
+        </Modal>
+    );
+}
