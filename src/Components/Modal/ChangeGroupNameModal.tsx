cf8af68db63f4cc9a802a50cd9f2f45f1d779cab
mrboparai <karanvirboparai123@gmail.com>
2022-10-23 09:57:23 -0400

    added modal input

--- src/Components/Modal/ChangeGroupNameModal.tsx
+++ src/Components/Modal/ChangeGroupNameModal.tsx
@@ -3,6 +3,7 @@ import { useAppDispatch } from "../../Hooks/reduxHooks";
 import { resetModal } from "../../Redux/slices/modalSlice";
 import BtnCallToAction from "../Buttons/BtnCallToAction";
 import BtnCancelAction from "../Buttons/BtnCancelAction";
+import ModalInput from "../Inputs/ModalInput";
 import Modal from "./Modal";
 
 // TODO give group id
@@ -13,32 +14,19 @@ export default function ChangeGroupNameModal() {
     return (
         <Modal modalName='Change Group Name'>
             <div className='flex flex-col gap-4 mt-6'>
-                <div className='form-control w-full'>
-                    <label className='label' htmlFor='prevName'>
-                        <span className='label-text text-white'>
-                            Previous Name
-                        </span>
-                    </label>
-                    <input
-                        id='prevName'
-                        type='text'
-                        placeholder=''
-                        className='input input-bordered bg-[#2A303C] w-full'
-                        contentEditable={false}
-                    />
-                </div>
-                <div className='form-control w-full'>
-                    <label className='label' htmlFor='newName'>
-                        <span className='label-text text-white'>New Name</span>
-                    </label>
-                    <input
-                        id='newName'
-                        type='text'
-                        placeholder='New group name'
-                        className='input input-bordered bg-[#2A303C] w-full'
-                        onChange={handleChange}
-                    />
-                </div>
+                <ModalInput
+                    labelName='Previous Name'
+                    editable={false}
+                    value={""}
+                    inputId='prevName'
+                ></ModalInput>
+                <ModalInput
+                    labelName='New Name'
+                    onChange={handleChange}
+                    value={newName}
+                    placeholder='New Group Name'
+                    inputId='newName'
+                ></ModalInput>
                 <div className='flex gap-4 mt-2'>
                     <BtnCallToAction
                         text='Change'
@@ -63,4 +51,35 @@ export default function ChangeGroupNameModal() {
 
     // api slice
     function handleSubmit() {}
+}
+
+{
+    /* <div className='form-control w-full'>
+                        <label className='label' htmlFor='prevName'>
+                            <span className='label-text text-white'>
+                                Previous Name
+                            </span>
+                        </label>
+                        <input
+                            id='prevName'
+                            type='text'
+                            placeholder=''
+                            className='input input-bordered bg-[#2A303C] w-full'
+                            contentEditable={false}
+                        />
+                    </div> */
+}
+{
+    /* <div className='form-control w-full'>
+                    <label className='label' htmlFor='newName'>
+                        <span className='label-text text-white'>New Name</span>
+                    </label>
+                    <input
+                        id='newName'
+                        type='text'
+                        placeholder='New group name'
+                        className='input input-bordered bg-[#2A303C] w-full'
+                        onChange={handleChange}
+                    />
+                </div> */
 }
