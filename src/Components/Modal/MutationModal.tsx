688670482633cbd698f974caadf6005cc887a7fa
mrboparai <karanvirboparai123@gmail.com>
2022-10-23 11:43:49 -0400

    added css

--- src/Components/Modal/MutationModal.tsx
+++ src/Components/Modal/MutationModal.tsx
@@ -21,12 +21,14 @@ export default function MutationModal({
     text,
 }: props) {
     return (
-        <Modal modalName={modalName}>
-            <div className='flex flex-col gap-4 mt-6'>
-                <label className='label flex gap-4'>
-                    <span className='label-text text-white'>{text}</span>
+        <Modal modalName={modalName} modalClass='!max-w-[300px]'>
+            <div className='flex flex-col flex-grow justify-end mt-6'>
+                <label className='label flex gap-4 mr-auto flex-grow'>
+                    <span className='label-text text-lg text-white'>
+                        {text}
+                    </span>
                 </label>
-                <div className='flex gap-4 mt-2'>
+                <div className='flex gap-4 mt-2s'>
                     <BtnCallToAction
                         onClick={handleSubmit}
                         text={btnCTAName}
