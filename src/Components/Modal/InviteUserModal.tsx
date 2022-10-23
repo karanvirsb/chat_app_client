d39f6576992573d8ed84d2c9d0d0a5cbd4cbc3e0
mrboparai <karanvirboparai123@gmail.com>
2022-10-23 10:09:31 -0400

    added input modal for invite code url

--- src/Components/Modal/InviteUserModal.tsx
+++ src/Components/Modal/InviteUserModal.tsx
@@ -1,10 +1,31 @@
 import React from "react";
+import BtnCallToAction from "../Buttons/BtnCallToAction";
+import ModalInput from "../Inputs/ModalInput";
 import Modal from "./Modal";
 
 export default function InviteUserModal() {
     return (
         <Modal modalName='Invite User'>
-            <div className='flex flex-col gap-4 mt-6'></div>
+            <div className='flex flex-col gap-4 mt-6'>
+                {/* TODO add invite url */}
+                <ModalInput
+                    inputId='inviteCode'
+                    labelName='Invite Code'
+                    editable={false}
+                    placeholder=''
+                    value=''
+                >
+                    <BtnCallToAction
+                        text='Copy'
+                        onClick={handleCopyInviteCode}
+                    ></BtnCallToAction>
+                </ModalInput>
+            </div>
         </Modal>
     );
+
+    function handleCopyInviteCode() {
+        const text = "copied code"; // TODO replace with invite code url
+        navigator.clipboard.writeText(text);
+    }
 }
