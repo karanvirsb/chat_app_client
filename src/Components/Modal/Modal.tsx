d7e07683ea74a6690acb855d94122c64d6d5a990
mrboparai <karanvirboparai123@gmail.com>
2022-10-23 08:51:19 -0400

    added screen sizes for large and xl. Added bg color

--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -18,7 +18,7 @@ const Modal = ({
             className={`modal modal-bottom sm:modal-middle ${bgClass} visible opacity-100 z-50 pointer-events-auto`}
         >
             <div
-                className={`modal-box relative ${modalClass} visible opacity-100 z-50`}
+                className={`modal-box bg-[#343C4B] relative lg:max-w-[650px] lg:min-h-[300px] xl:max-w-[700px] xl:min-h-[350px] ${modalClass} visible opacity-100 z-50`}
             >
                 <div className='absolute top-[2px] left-[10px]'>
                     {modalName}
