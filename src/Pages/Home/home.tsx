12b9dd31603397540947d9ee6c99ba7cf44f98fd
mrboparai <karanvirboparai123@gmail.com>
2022-10-24 08:35:57 -0400

    added group top bar

--- src/Pages/Home/home.tsx
+++ src/Pages/Home/home.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useSessionContext } from "supertokens-auth-react/recipe/session";
 import Chat from "./Components/Chat";
+import GroupTopBar from "./Components/GroupTopBar";
 import Sidebar from "./Components/Sidebar";
 import SidebarInfo from "./Components/SidebarInfo";
 import Users from "./Components/Users";
@@ -35,78 +36,12 @@ export default function Home() {
                 {isSideBarOpen && <SidebarInfo></SidebarInfo>}
                 {/* Top Bar */}
                 <div className='flex flex-col flex-grow'>
-                    <div className='bg-chat-bg border-b border-r-0 border-groupBar-bg flex items-center justify-between font-semibold drop-shadow-md py-2 px-4 w-full h-16 text-white'>
-                        <div className='flex gap-4'>
-                            <button onClick={toggleSideBar}>
-                                {isSideBarOpen ? (
-                                    <svg
-                                        xmlns='http://www.w3.org/2000/svg'
-                                        fill='none'
-                                        viewBox='0 0 24 24'
-                                        strokeWidth={1.5}
-                                        stroke='currentColor'
-                                        className='w-6 h-6 hover:text-black'
-                                    >
-                                        <path
-                                            strokeLinecap='round'
-                                            strokeLinejoin='round'
-                                            d='M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75'
-                                        />
-                                    </svg>
-                                ) : (
-                                    <svg
-                                        xmlns='http://www.w3.org/2000/svg'
-                                        fill='none'
-                                        viewBox='0 0 24 24'
-                                        strokeWidth={1.5}
-                                        stroke='currentColor'
-                                        className='w-6 h-6 hover:text-black'
-                                    >
-                                        <path
-                                            strokeLinecap='round'
-                                            strokeLinejoin='round'
-                                            d='M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9'
-                                        />
-                                    </svg>
-                                )}
-                            </button>
-                            {/*TODO Channel name goes here*/}
-                            <span>General</span>
-                        </div>
-                        <button onClick={toggleUserMenu}>
-                            {isUserMenuOpen ? (
-                                <svg
-                                    xmlns='http://www.w3.org/2000/svg'
-                                    fill='none'
-                                    viewBox='0 0 24 24'
-                                    strokeWidth={1.5}
-                                    stroke='currentColor'
-                                    className='w-6 h-6'
-                                >
-                                    <path
-                                        strokeLinecap='round'
-                                        strokeLinejoin='round'
-                                        d='M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z'
-                                    />
-                                </svg>
-                            ) : (
-                                <svg
-                                    xmlns='http://www.w3.org/2000/svg'
-                                    fill='none'
-                                    viewBox='0 0 24 24'
-                                    strokeWidth={1.5}
-                                    stroke='currentColor'
-                                    className='w-6 h-6 text-gray-400'
-                                >
-                                    <path
-                                        strokeLinecap='round'
-                                        strokeLinejoin='round'
-                                        d='M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z'
-                                    />
-                                </svg>
-                            )}
-                        </button>
-                    </div>
+                    <GroupTopBar
+                        isSideBarOpen={isSideBarOpen}
+                        isUserMenuOpen={isUserMenuOpen}
+                        toggleSideBar={toggleSideBar}
+                        toggleUserMenu={toggleUserMenu}
+                    ></GroupTopBar>
                     <div className='flex flex-grow'>
                         <Chat></Chat>
                         {isUserMenuOpen && <Users></Users>}
