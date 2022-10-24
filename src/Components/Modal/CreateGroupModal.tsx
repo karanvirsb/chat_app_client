498bbda519fb88f530c34f0564e4524e91acb8b5
mrboparai <karanvirboparai123@gmail.com>
2022-10-24 10:11:55 -0400

    created the create group modal

--- src/Components/Modal/CreateGroupModal.tsx
+++ src/Components/Modal/CreateGroupModal.tsx
@@ -1,6 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAppDispatch } from "../../Hooks/reduxHooks";
+import { resetModal } from "../../Redux/slices/modalSlice";
+import BtnCallToAction from "../Buttons/BtnCallToAction";
+import BtnCancelAction from "../Buttons/BtnCancelAction";
+import ModalInput from "../Inputs/ModalInput";
+import Modal from "./Modal";
 
 // TODO
 export default function CreateGroupModal() {
-    return <div>CreateGroupModal</div>;
+    const [groupName, setGroupName] = useState("");
+    const dispatch = useAppDispatch();
+    return (
+        <Modal modalName='Change Group Name'>
+            <div className='flex flex-col gap-4 mt-6'>
+                <ModalInput
+                    labelName='New Name'
+                    onChange={handleChange}
+                    value={groupName}
+                    placeholder='Group Name'
+                    inputId='groupName'
+                ></ModalInput>
+                <div className='flex gap-4 mt-2'>
+                    <BtnCallToAction
+                        text='Change'
+                        onClick={handleSubmit}
+                    ></BtnCallToAction>
+                    <BtnCancelAction
+                        text='Cancel'
+                        onClick={closeModal}
+                    ></BtnCancelAction>
+                </div>
+            </div>
+        </Modal>
+    );
+
+    function closeModal() {
+        dispatch(resetModal());
+    }
+
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {}
+
+    function handleSubmit() {}
 }
