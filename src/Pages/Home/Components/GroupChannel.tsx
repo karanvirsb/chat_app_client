43c5722e2328bf7f52ed300021d0028d6a200140
mrboparai <karanvirboparai123@gmail.com>
2022-10-25 10:20:45 -0400

    added group bar, chat and users to channel

--- src/Pages/Home/Components/GroupChannel.tsx
+++ src/Pages/Home/Components/GroupChannel.tsx
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import GroupChat from "./GroupChat";
+import GroupSidebarInfo from "./GroupSidebarInfo";
+import GroupTopBar from "./GroupTopBar";
+import GroupUsers from "./GroupUsers";
 
 export default function GroupChannel() {
-    return <div>GroupChannel</div>;
+    const [isSideBarOpen, setIsSideBarOpen] = useState(true);
+    const [isUserMenuOpen, setIsUserMenuOpen] = useState(true);
+
+    return (
+        <>
+            {isSideBarOpen && <GroupSidebarInfo></GroupSidebarInfo>}
+            <div className='flex flex-col flex-grow'>
+                <GroupTopBar
+                    isSideBarOpen={isSideBarOpen}
+                    isUserMenuOpen={isUserMenuOpen}
+                    toggleSideBar={toggleSideBar}
+                    toggleUserMenu={toggleUserMenu}
+                ></GroupTopBar>
+                <div className='flex flex-grow'>
+                    <GroupChat></GroupChat>
+                    {isUserMenuOpen && <GroupUsers></GroupUsers>}
+                </div>
+            </div>
+        </>
+    );
+
+    function toggleSideBar() {
+        setIsSideBarOpen((prev) => !prev);
+    }
+
+    function toggleUserMenu() {
+        setIsUserMenuOpen((prev) => !prev);
+    }
 }
