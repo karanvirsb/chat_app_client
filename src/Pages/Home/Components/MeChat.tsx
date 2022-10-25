fa1f55fe954b086c4a133aa76184bad92bd9284e
mrboparai <karanvirboparai123@gmail.com>
2022-10-25 08:59:23 -0400

    created a chat for me channel

---
+++ src/Pages/Home/Components/MeChat.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function MeChat() {
+    return (
+        <div className='bg-chat-bg flex flex-col flex-grow h-full'>
+            <div className='flex-grow p-4 w-full '>
+                {/* TODO Create chat component */}
+            </div>
+            <div className='input-group p-4'>
+                <input
+                    type='text'
+                    placeholder='Send a message'
+                    className='input input-bordered bg-[#2a303c] w-full focus:outline-none'
+                />
+            </div>
+        </div>
+    );
+}
