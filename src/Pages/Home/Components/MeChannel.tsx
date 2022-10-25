52f6916ec3c39e78f54dee1ac0f7583059f5b4a9
mrboparai <karanvirboparai123@gmail.com>
2022-10-25 10:25:41 -0400

    created a me channel

--- src/Pages/Home/Components/MeChannel.tsx
+++ src/Pages/Home/Components/MeChannel.tsx
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import MeChat from "./MeChat";
+import MeSideBarInfo from "./MeSideBarInfo";
+import MeTopBar from "./MeTopBar";
 
 export default function MeChannel() {
-    return <div>MeChannel</div>;
+    const [isSideBarOpen, setIsSideBarOpen] = useState(true);
+    return (
+        <>
+            {isSideBarOpen && <MeSideBarInfo></MeSideBarInfo>}
+            <div className='flex flex-col flex-grow'>
+                <MeTopBar
+                    isSideBarOpen={isSideBarOpen}
+                    toggleSideBar={toggleSideBar}
+                ></MeTopBar>
+                <div className='flex flex-grow'>
+                    <MeChat></MeChat>
+                </div>
+            </div>
+        </>
+    );
+
+    function toggleSideBar() {
+        setIsSideBarOpen((prev) => !prev);
+    }
 }
