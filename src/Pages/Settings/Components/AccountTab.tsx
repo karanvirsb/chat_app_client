620bf7bca55bfc3e35d09dcae1ff2cacb6aba248
mrboparai <karanvirboparai123@gmail.com>
2022-10-26 10:59:24 -0400

    added flex and user input

--- src/Pages/Settings/Components/AccountTab.tsx
+++ src/Pages/Settings/Components/AccountTab.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import UserInput from "./userInput";
 
 // TODO add username, email, password info and edit btn
 // TODO add delete account button
-const AccountTab = () => {
-    return <div>AccountTab</div>;
-};
-
-export default AccountTab;
+export default function AccountTab() {
+    return (
+        <div className='flex flex-col items-center w-full'>
+            <UserInput
+                labelName='Username'
+                value='username'
+                onClick={() => {}}
+            ></UserInput>
+        </div>
+    );
+}
