61dd62434e1d3b6f4cb050f1c57b7d8bb65671fe
mrboparai <karanvirboparai123@gmail.com>
2022-10-26 10:15:18 -0400

    created a side bar with tabs

--- src/Pages/Settings/Components/Sidebar.tsx
+++ src/Pages/Settings/Components/Sidebar.tsx
@@ -2,8 +2,25 @@ import React from "react";
 
 // TODO Add account settings and back button
 // TODO add tab display
-const Sidebar = () => {
-    return <div>Sidebar</div>;
-};
-
-export default Sidebar;
+export default function Sidebar() {
+    const tabs = ["Account"];
+    return (
+        <nav className='flex flex-col bg-groupInfo-bg max-w-[250px] h-screen'>
+            <button>Back</button>
+            <div>
+                <h2>User Settings</h2>
+                <ul className='w-full'>
+                    {tabs.map((tab, index) => {
+                        return (
+                            <li className='w-full text-center '>
+                                <a className='tab hover:bg-[#2A303C] text-[#DFDFDF] w-full'>
+                                    {tab}
+                                </a>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        </nav>
+    );
+}
