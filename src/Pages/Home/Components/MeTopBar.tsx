e014f2a6acbe8a5140082cdfdd6fed84ce7731ef
mrboparai <karanvirboparai123@gmail.com>
2022-10-26 08:39:20 -0400

    added function to display modal

--- src/Pages/Home/Components/MeTopBar.tsx
+++ src/Pages/Home/Components/MeTopBar.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useAppDispatch } from "../../../Hooks/reduxHooks";
+import { setModal } from "../../../Redux/slices/modalSlice";
 
 type props = {
     isSideBarOpen: boolean;
@@ -6,6 +8,7 @@ type props = {
 };
 
 export default function MeTopBar({ isSideBarOpen, toggleSideBar }: props) {
+    const dispatch = useAppDispatch();
     return (
         <div className='bg-chat-bg border-b border-r-0 border-groupBar-bg flex items-center justify-between font-semibold drop-shadow-md py-2 px-4 w-full h-16 text-white'>
             <div className='flex gap-4 items-center'>
@@ -44,10 +47,23 @@ export default function MeTopBar({ isSideBarOpen, toggleSideBar }: props) {
                 </button>
                 {/*TODO Friends username goes here*/}
                 <span>Friends Name</span>
-                <button className='btn btn-sm border-none bg-accent-color font-bold ml-4 text-black hover:bg-accent-hover'>
+                <button
+                    className='btn btn-sm border-none bg-accent-color font-bold ml-4 text-black hover:bg-accent-hover'
+                    onClick={displayAddFriendModal}
+                >
                     Add Friends
                 </button>
             </div>
         </div>
     );
+
+    function displayAddFriendModal() {
+        dispatch(
+            setModal({
+                modalName: "addFriend",
+                open: true,
+                options: {},
+            })
+        );
+    }
 }
