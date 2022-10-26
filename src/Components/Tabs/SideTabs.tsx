e4f1190471e11de5529006fa0ddf344f43654139
mrboparai <karanvirboparai123@gmail.com>
2022-10-26 09:53:50 -0400

    created side tabs

---
+++ src/Components/Tabs/SideTabs.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+type props = {
+    components: Map<String, JSX.Element>;
+    tabs: string[];
+};
+
+export default function SideTabs({ components, tabs }: props) {
+    const [activeIndex, setActiveIndex] = useState(0);
+    return (
+        <>
+            <div className={`flex flex-col w-full p-[1px] overflow-auto`}>
+                {tabs.map((tab, index) => {
+                    if (index === activeIndex) {
+                        return (
+                            <button
+                                className='btn flex-grow rounded-none'
+                                onClick={() => setTab(index)}
+                            >
+                                {tab}
+                            </button>
+                        );
+                    } else {
+                        return (
+                            <button
+                                className='btn flex-grow rounded-none'
+                                onClick={() => setTab(index)}
+                            >
+                                {tab}
+                            </button>
+                        );
+                    }
+                })}
+            </div>
+            <article>{components.get(tabs[activeIndex].toLowerCase())}</article>
+        </>
+    );
+
+    function setTab(index: number) {
+        setActiveIndex(() => index);
+    }
+}
