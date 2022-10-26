3519d0d27ad575231b347df4768da6bc0643c208
mrboparai <karanvirboparai123@gmail.com>
2022-10-26 10:40:23 -0400

    created tabs array along with activeIndex

--- src/Pages/Settings/settings.tsx
+++ src/Pages/Settings/settings.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import AccountTab from "./Components/AccountTab";
 import Sidebar from "./Components/Sidebar";
 
 const settings = () => {
-    return <Sidebar></Sidebar>;
+    const tabs = ["Accounts"];
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    return (
+        <div className='flex'>
+            <Sidebar
+                tabs={tabs}
+                activeIndex={activeIndex}
+                setActiveIndex={setActiveIndex}
+            ></Sidebar>
+            {activeIndex === 0 && <AccountTab></AccountTab>}
+        </div>
+    );
 };
 
 export default settings;
