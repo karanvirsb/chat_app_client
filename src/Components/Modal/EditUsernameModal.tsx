ace9eeba2fc86398917aaacf25ea72c6c4533606
mrboparai <karanvirboparai123@gmail.com>
2022-10-27 09:22:58 -0400

    created an edit user name modal

---
+++ src/Components/Modal/EditUsernameModal.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { useAppDispatch } from "../../Hooks/reduxHooks";
+import { resetModal } from "../../Redux/slices/modalSlice";
+import BtnCallToAction from "../Buttons/BtnCallToAction";
+import BtnCancelAction from "../Buttons/BtnCancelAction";
+import ModalInput from "../Inputs/ModalInput";
+import Modal from "./Modal";
+
+export default function EditUsernameModal() {
+    const [errorMsg, setErrorMsg] = useState("");
+    const [newUsername, setNewUsername] = useState("");
+    const [currPassword, setCurrPassword] = useState("");
+
+    const dispatch = useAppDispatch();
+
+    return (
+        <Modal modalName='Change Username'>
+            <div className='flex flex-col gap-4 mt-6'>
+                <ModalInput
+                    inputId='currPassword'
+                    labelName='Current Password'
+                    errorMsg={errorMsg}
+                ></ModalInput>
+                <ModalInput
+                    inputId='username'
+                    labelName='New Username'
+                    errorMsg={errorMsg}
+                ></ModalInput>
+                <div className='flex gap-4 mt-2'>
+                    <BtnCallToAction
+                        onClick={handleSubmit}
+                        text='Change'
+                    ></BtnCallToAction>
+                    <BtnCancelAction
+                        onClick={handleCancel}
+                        text='Cancel'
+                    ></BtnCancelAction>
+                </div>
+            </div>
+        </Modal>
+    );
+
+    function handleSubmit() {}
+
+    function handleCancel() {
+        dispatch(resetModal());
+    }
+}
