e694f6c23f95fd34b84441623b7e80f65f92c2b6
mrboparai <karanvirboparai123@gmail.com>
2022-10-27 08:31:14 -0400

    added another div and some css

--- src/Pages/Settings/Components/AccountTab.tsx
+++ src/Pages/Settings/Components/AccountTab.tsx
@@ -5,24 +5,28 @@ import UserInput from "./UserInput";
 // TODO add delete account button
 export default function AccountTab() {
     return (
-        <div className='flex flex-col items-center w-full'>
-            <h1>Account</h1>
-            <UserInput
-                labelName='Username'
-                value='username'
-                onClick={() => {}}
-            ></UserInput>
-            <UserInput
-                labelName='Email'
-                value='Email'
-                onClick={() => {}}
-            ></UserInput>
-            <UserInput
-                labelName='Password'
-                value='password'
-                inputType='password'
-                onClick={() => {}}
-            ></UserInput>
+        <div className='flex flex-col gap-4 items-center w-full'>
+            <h1 className='font-semibold px-4 py-2 text-gray-600 text-lg text-left uppercase w-full'>
+                Account
+            </h1>
+            <div className='bg-[hsl(220,18%,15%)] flex flex-col gap-4 items-center py-4 rounded-md min-w-[150px] w-[70%] max-w-[700px]'>
+                <UserInput
+                    labelName='Username'
+                    value='username'
+                    onClick={() => {}}
+                ></UserInput>
+                <UserInput
+                    labelName='Email'
+                    value='Email'
+                    onClick={() => {}}
+                ></UserInput>
+                <UserInput
+                    labelName='Password'
+                    value='password'
+                    inputType='password'
+                    onClick={() => {}}
+                ></UserInput>
+            </div>
             <button className='btn bg-btn-mutations border-none self-start mt-6 text-btn-mutations-text hover:bg-btn-mutations-hover'>
                 Delete Account
             </button>
