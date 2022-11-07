ee8af1e90a51c0d6e133545d9ef19081edd31223
mrboparai <karanvirboparai123@gmail.com>
2022-11-07 10:27:40 -0500

    added x mark

--- src/Pages/Home/Components/GroupUsers.tsx
+++ src/Pages/Home/Components/GroupUsers.tsx
@@ -26,7 +26,22 @@ export default function GroupUsers({ isUserMenuOpen, toggleUserMenu }: props) {
             <button
                 className='btn btn-circle absolute bottom-[10px] right-[10px]'
                 onClick={toggleUserMenu}
-            ></button>
+            >
+                <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    strokeWidth={1.5}
+                    stroke='currentColor'
+                    className='w-6 h-6'
+                >
+                    <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        d='M6 18L18 6M6 6l12 12'
+                    />
+                </svg>
+            </button>
         </article>
     );
 
