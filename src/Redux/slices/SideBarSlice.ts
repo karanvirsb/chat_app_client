04bd15d13c67b3ae73d843c250171e823fd0ec15
mrboparai <karanvirboparai123@gmail.com>
2022-11-07 09:51:54 -0500

    created a side bar slice

---
+++ src/Redux/slices/SideBarSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// Define a type for the slice state
+interface SideBarState {
+    open: boolean;
+}
+
+// Define the initial state using that type
+const initialState: SideBarState = {
+    open: false,
+};
+
+export const sideBarSlice = createSlice({
+    name: "sideBar",
+    // `createSlice` will infer the state type from the `initialState` argument
+    initialState,
+    reducers: {
+        setSideBarOpen: (state) => {
+            state.open = true;
+        },
+        setSideBarClosed: (state) => {
+            state.open = false;
+        },
+    },
+});
+
+export const { setSideBarOpen, setSideBarClosed } = sideBarSlice.actions;
+
+// Other code such as selectors can use the imported `RootState` type
+// export const selectSideBarState = (state: RootState) => state.counter.value;
+
+export default sideBarSlice.reducer;
