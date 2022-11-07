52022500dd7df1788111801a0d1fdee0d5682aeb
mrboparai <karanvirboparai123@gmail.com>
2022-11-07 09:05:08 -0500

    made bottom bar fixed

--- src/Components/BottomBar/BottomBar.tsx
+++ src/Components/BottomBar/BottomBar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function BottomBar() {
     return (
-        <nav className='absolute bottom-0 left-0 bg-groupBar-bg flex justify-around min-h-[75px] w-full m-sm:hidden'>
+        <nav className='fixed bottom-0 left-0 bg-groupBar-bg flex justify-around min-h-[75px] w-full m-sm:hidden'>
             {/* CHAT ICON */}
             <button>
                 <svg
