7d7f7cdc60b612bee0ca45d7e140d7d7f80ae423
mrboparai <karanvirboparai123@gmail.com>
2022-11-09 09:58:32 -0500

    added hiding of the side bar

--- src/Pages/Home/Components/MeSideBarInfo.tsx
+++ src/Pages/Home/Components/MeSideBarInfo.tsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { useAppDispatch } from "../../../Hooks/reduxHooks";
+import { useAppDispatch, useAppSelector } from "../../../Hooks/reduxHooks";
 
 // TODO gather user id and then map through friends list
 export default function MeSideBarInfo() {
     const dispatch = useAppDispatch();
+    const isSideBarOpen = useAppSelector((state) => state.SideBarReducer.open);
     return (
-        <div className='flex flex-col md:min-w-[235.375px]'>
+        <div
+            className={`flex flex-col min-w-[250px] sm:fixed sm:top-0 sm:left-[72px] sm:bottom-0 sm:z-[5] sm:${
+                isSideBarOpen ? "translate-x-0" : "-translate-x-[100%]"
+            } `}
+        >
             <h1 className='bg-groupInfo-bg border-b border-chat-bg flex items-center font-semibold drop-shadow-md py-2 px-4 w-full h-16 text-white'>
                 Friends
             </h1>
