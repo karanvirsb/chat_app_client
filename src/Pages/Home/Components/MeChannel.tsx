1be9f3388b280cc4bd6b17927b8d9ed1d58055d2
mrboparai <karanvirboparai123@gmail.com>
2022-11-09 09:51:14 -0500

    removed useState side bar with selector and added css

--- src/Pages/Home/Components/MeChannel.tsx
+++ src/Pages/Home/Components/MeChannel.tsx
@@ -1,26 +1,27 @@
 import React, { useState } from "react";
+import { useAppSelector } from "../../../Hooks/reduxHooks";
 import MeChat from "./MeChat";
 import MeSideBarInfo from "./MeSideBarInfo";
 import MeTopBar from "./MeTopBar";
 
 export default function MeChannel() {
-    const [isSideBarOpen, setIsSideBarOpen] = useState(true);
+    // const [isSideBarOpen, setIsSideBarOpen] = useState(true);
+    const isSideBarOpen = useAppSelector((state) => state.SideBarReducer.open);
     return (
         <>
             {isSideBarOpen && <MeSideBarInfo></MeSideBarInfo>}
-            <div className='flex flex-col flex-grow'>
-                <MeTopBar
-                    isSideBarOpen={isSideBarOpen}
-                    toggleSideBar={toggleSideBar}
-                ></MeTopBar>
+            <div
+                className={`flex flex-col flex-grow ${
+                    isSideBarOpen
+                        ? "sm:translate-x-[307.375px]"
+                        : "sm:translate-x-[0px]"
+                }`}
+            >
+                <MeTopBar></MeTopBar>
                 <div className='flex flex-grow'>
                     <MeChat></MeChat>
                 </div>
             </div>
         </>
     );
-
-    function toggleSideBar() {
-        setIsSideBarOpen((prev) => !prev);
-    }
 }
