76fd223d4ef6fc766b4130e19e11e791ee7536d1
mrboparai <karanvirboparai123@gmail.com>
2022-11-10 09:04:26 -0500

    added channel container

--- src/Pages/Home/Components/MeChannel.tsx
+++ src/Pages/Home/Components/MeChannel.tsx
@@ -1,27 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
+import ChannelContainer from "../../../Components/ChannelContainer/ChannelContainer";
 import { useAppSelector } from "../../../Hooks/reduxHooks";
 import MeChat from "./MeChat";
 import MeSideBarInfo from "./MeSideBarInfo";
 import MeTopBar from "./MeTopBar";
 
 export default function MeChannel() {
-    // const [isSideBarOpen, setIsSideBarOpen] = useState(true);
     const isSideBarOpen = useAppSelector((state) => state.SideBarReducer.open);
     return (
         <>
             {isSideBarOpen && <MeSideBarInfo></MeSideBarInfo>}
-            <div
-                className={`flex flex-col flex-grow ${
-                    isSideBarOpen
-                        ? "sm:translate-x-[307.375px]"
-                        : "sm:translate-x-[0px]"
-                }`}
-            >
-                <MeTopBar></MeTopBar>
-                <div className='flex flex-grow'>
-                    <MeChat></MeChat>
-                </div>
-            </div>
+            <ChannelContainer>
+                <>
+                    <MeTopBar></MeTopBar>
+                    <div className='flex flex-grow'>
+                        <MeChat></MeChat>
+                    </div>
+                </>
+            </ChannelContainer>
         </>
     );
 }
