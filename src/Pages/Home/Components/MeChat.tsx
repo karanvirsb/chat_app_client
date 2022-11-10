1bae4c039831441419ac7347f81cb7120f5a31c6
mrboparai <karanvirboparai123@gmail.com>
2022-11-10 09:06:32 -0500

    added todo

--- src/Pages/Home/Components/MeChat.tsx
+++ src/Pages/Home/Components/MeChat.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+// TODO create one chat component
 export default function MeChat() {
     return (
         <div className='bg-chat-bg flex flex-col flex-grow h-full'>
