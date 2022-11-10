58b562f2e3303a781cf56cb5332c2dbde5c92e85
mrboparai <karanvirboparai123@gmail.com>
2022-11-10 09:34:07 -0500

    added top bar container

--- src/Pages/Home/Components/MeTopBar.tsx
+++ src/Pages/Home/Components/MeTopBar.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ToggleSidebarBtn from "../../../Components/ToggleSidebarBtn/ToggleSidebarBtn";
+import TopBarContainer from "../../../Components/TopBarContainer/TopBarContainer";
 import { useAppDispatch } from "../../../Hooks/reduxHooks";
 import { setModal } from "../../../Redux/slices/modalSlice";
 
 export default function MeTopBar() {
     const dispatch = useAppDispatch();
     return (
-        <div className='bg-chat-bg border-b border-r-0 border-groupBar-bg flex items-center justify-between font-semibold drop-shadow-md py-2 px-4 w-full h-16 text-white'>
+        <TopBarContainer>
             <div className='flex gap-4 items-center'>
                 <ToggleSidebarBtn></ToggleSidebarBtn>
                 {/*TODO Friends username goes here*/}
@@ -18,7 +19,7 @@ export default function MeTopBar() {
                     Add Friends
                 </button>
             </div>
-        </div>
+        </TopBarContainer>
     );
 
     function displayAddFriendModal() {
