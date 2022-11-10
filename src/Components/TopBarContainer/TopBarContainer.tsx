9506cd2406ba38a9ec8157891d45010745e58555
mrboparai <karanvirboparai123@gmail.com>
2022-11-10 09:32:26 -0500

    renamed

--- src/Components/TopBarContainer/TopBarContainer.tsx
+++ src/Components/TopBarContainer/TopBarContainer.tsx
@@ -4,7 +4,7 @@ type props = {
     children: JSX.Element;
 };
 
-export default function TopBar({ children }: props) {
+export default function TopBarContainer({ children }: props) {
     return (
         <div className='bg-chat-bg border-b border-r-0 border-groupBar-bg flex items-center justify-between font-semibold drop-shadow-md py-2 px-4 w-full h-16 text-white'>
             {children}
