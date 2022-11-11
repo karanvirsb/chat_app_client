29075c00c004ff5b444f53224e8c303127787738
mrboparai <karanvirboparai123@gmail.com>
2022-11-11 10:47:28 -0500

    created an group Api slice

---
+++ src/Redux/slices/groupApiSlice.ts
@@ -0,0 +1,22 @@
+// Import the RTK Query methods from the React-specific entry point
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export interface IGroup {
+    groupName: string;
+    groupId: string;
+    inviteCode: string;
+    dateCreated: Date;
+}
+
+export const groupApiSlice = createApi({
+    reducerPath: "groups",
+    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000/group" }),
+    tagTypes: ["Groups"],
+    endpoints: (builder) => ({
+        getGroups: builder.query<IGroup, string>({
+            query: (userId) => ({ url: `userId/${userId}` }),
+        }),
+    }),
+});
+
+export const { useGetGroupsQuery } = groupApiSlice;
