de907350e37b9c7b0b521532a122befd5a481b90
mrboparai <karanvirboparai123@gmail.com>
2022-11-14 11:40:08 -0500

    removed session into hook

--- src/Pages/Home/home.tsx
+++ src/Pages/Home/home.tsx
@@ -1,30 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { useSessionContext } from "supertokens-auth-react/recipe/session";
+import React, { useState } from "react";
 import Sidebar from "./Components/Sidebar";
 import GroupChannel from "./Components/GroupChannel";
 import MeChannel from "./Components/MeChannel";
 
 export default function Home() {
-    const [sessionInfo, setSessionInfo] = useState<{
-        doesSessionExist: boolean;
-        userId: string;
-        accessTokenPayload: any;
-    } | null>(null);
-    let session = useSessionContext();
-
     const [tab, setTab] = useState<"group" | "me">("me");
     const [tabId, setTabId] = useState(""); // can be a userId or groupId
-
-    useEffect(() => {
-        if (!session.loading) {
-            let { doesSessionExist, userId, accessTokenPayload } = session;
-            setSessionInfo({
-                doesSessionExist,
-                userId,
-                accessTokenPayload,
-            });
-        }
-    }, [session.loading]);
 
     return (
         <>
