859fbd8997f7b8c996c463671e8d20c525a4eec5
mrboparai <karanvirboparai123@gmail.com>
2022-11-15 13:36:35 -0500

    added loading state to btn

--- src/Components/Buttons/BtnCallToAction.tsx
+++ src/Components/Buttons/BtnCallToAction.tsx
@@ -4,15 +4,22 @@ type props = {
     text: string;
     onClick: (...args: any) => any;
     classname?: string;
+    isLoading?: boolean;
 };
 
-export default function BtnCallToAction({ text, onClick, classname }: props) {
+export default function BtnCallToAction({
+    text,
+    onClick,
+    classname,
+    isLoading,
+}: props) {
     return (
         <button
             className={`btn bg-btn-cta  border-none font-bold tracking-wider hover:bg-btn-cta-hover text-white ${classname}`}
             onClick={onClick}
         >
-            {text}
+            // TODO add spinner
+            {isLoading ? "Loading" : text}
         </button>
     );
 }
