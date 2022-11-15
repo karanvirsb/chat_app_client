9e630dfab10bb02c74c2349dea4689ee12a76e25
mrboparai <karanvirboparai123@gmail.com>
2022-11-15 16:08:20 -0500

    removed on small screen

--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -15,7 +15,7 @@ const Modal = ({
 }: props) => {
     return (
         <div
-            className={`modal modal-bottom sm:modal-middle ${bgClass} visible opacity-100 z-50 pointer-events-auto`}
+            className={`modal modal-middle ${bgClass} visible opacity-100 z-50 pointer-events-auto`}
         >
             <div
                 className={`modal-box bg-[#343C4B] relative lg:max-w-[650px] lg:min-h-[300px] xl:max-w-[700px] xl:min-h-[350px] ${modalClass} visible opacity-100 z-50`}
