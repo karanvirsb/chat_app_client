23724bf112e14466851462a72f349f57b185406a
mrboparai <karanvirboparai123@gmail.com>
2022-11-15 18:13:39 -0500

    added local storage check

--- src/Pages/Home/home.tsx
+++ src/Pages/Home/home.tsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import Sidebar from "./Components/Sidebar";
 import GroupChannel from "./Components/GroupChannel";
 import MeChannel from "./Components/MeChannel";
+import useLocalStorage from "../../Hooks/useLocalStorage";
 
 export default function Home() {
-    const [tab, setTab] = useState<"group" | "me">("me");
+    // const [tab, setTab] = useState<"group" | "me">("me");
+    const [tab, setTab] = useLocalStorage<"group" | "me">("groupOrMeTab", "me");
     const [tabId, setTabId] = useState(""); // can be a userId or groupId
 
     return (
