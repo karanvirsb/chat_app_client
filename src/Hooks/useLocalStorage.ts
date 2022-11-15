938648025f3c093913a4295351eb4caa9291ac34
mrboparai <karanvirboparai123@gmail.com>
2022-11-15 18:00:36 -0500

    created a hook for local storage

---
+++ src/Hooks/useLocalStorage.ts
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+
+export default function useLocalStorage<T>(key: string, initialValue: T) {
+    const [storedValue, setStoredValue] = useState<T>(() => {
+        let currentValue;
+
+        try {
+            currentValue = JSON.parse(
+                localStorage.getItem(key) || String(initialValue)
+            );
+        } catch (error) {
+            currentValue = initialValue;
+        }
+
+        return currentValue;
+    });
+
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(storedValue));
+    }, [storedValue, key]);
+
+    return { storedValue, setStoredValue };
+}
