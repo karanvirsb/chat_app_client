36b84a7f61e30e7f065c82a9a6f3f3a0f17eebe4
mrboparai <karanvirboparai123@gmail.com>
2022-11-15 16:41:43 -0500

    added a better spinner

--- src/Components/Spinner/Spinner.tsx
+++ src/Components/Spinner/Spinner.tsx
@@ -2,8 +2,19 @@ import React from "react";
 
 export default function Spinner() {
     return (
-        <div className='absolute right-1/2 bottom-1/2  transform translate-x-1/2 translate-y-1/2 '>
-            <div className='border-t-transparent border-solid animate-spin  rounded-full border-blue-400 border-8 h-64 w-64'></div>
-        </div>
+        <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='w-8 h-8 animate-spin'
+        >
+            <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99'
+            />
+        </svg>
     );
 }
