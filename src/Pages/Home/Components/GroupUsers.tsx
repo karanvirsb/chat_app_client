9e79a25da7a8156e0e221a71e09c2cde695eba53
mrboparai <karanvirboparai123@gmail.com>
2022-11-15 21:40:15 -0500

    removed todo

--- src/Pages/Home/Components/GroupUsers.tsx
+++ src/Pages/Home/Components/GroupUsers.tsx
@@ -22,7 +22,6 @@ export default function GroupUsers({
             {/* <div className='bg-chat-bg drop-shadow-md flex items-center font-semibold py-2 px-4 w-full h-16 text-white'></div> */}
             <div className='bg-groupInfo-bg text-white flex-grow'>
                 {/* TODO add top bar with search */}
-                {/* TODO create tabs for online and offline users */}
                 <Tabs
                     components={createComponents()}
                     tabs={createTabs()}
