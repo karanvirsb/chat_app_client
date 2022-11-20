65a01191173067c560bc617a3e77099b32d77d3f
mrboparai <karanvirboparai123@gmail.com>
2022-11-20 17:51:18 -0500

    created a copied pop up component

---
+++ src/Components/CopiedPopUp/CopiedPopUp.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+
+type props = {
+    copied: boolean;
+    setCopied: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export default function CopiedPopUp({ copied, setCopied }: props) {
+    useEffect(() => {
+        let timer: ReturnType<typeof setTimeout>;
+        if (copied) {
+            timer = setTimeout(() => {
+                setCopied(false);
+            }, 1500);
+        }
+
+        return () => clearTimeout(timer);
+    }, [copied]);
+
+    return copied ? (
+        <div className=' bg-[#121212] fixed top-1/2 left-1/2 font-semibold p-4 rounded-md opacity-80 z-50 '>
+            Copied
+        </div>
+    ) : null;
+}
