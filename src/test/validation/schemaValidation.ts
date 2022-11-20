24a4b490d5d7f171660960b051113acb8d05e5ee
mrboparai <karanvirboparai123@gmail.com>
2022-11-20 10:10:52 -0500

    added group validation

--- src/test/validation/schemaValidation.ts
+++ src/test/validation/schemaValidation.ts
@@ -3,3 +3,7 @@ import { IGroup } from "../../Redux/slices/groupApiSlice";
 export function isGroupArray(arr: IGroup[] | string): arr is IGroup[] {
     return (arr as IGroup[]).map !== undefined;
 }
+
+export function isGroup(item: IGroup | string | undefined): item is IGroup {
+    return (item as IGroup).groupId !== undefined;
+}
