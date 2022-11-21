c93404271d8275c3d24f0341fe203606415bb822
mrboparai <karanvirboparai123@gmail.com>
2022-11-21 12:15:19 -0500

    created a filter groups hook

---
+++ src/Pages/Home/Hooks/useFilterGroups.ts
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import { IGroup } from "../../../Redux/slices/groupApiSlice";
+import { isGroupArray } from "../../../test/validation/schemaValidation";
+
+type props = {
+    groups: IGroup[] | string | undefined;
+    groupId: string;
+};
+
+export default function useFilterGroups({ groups, groupId }: props): IGroup {
+    const [group, setGroup] = useState<IGroup>({
+        groupId: "",
+        dateCreated: new Date(),
+        groupName: "",
+        inviteCode: "",
+    });
+
+    useEffect(() => {
+        if (isGroupArray(groups)) {
+            const foundGroup = groups.find(
+                (group) => group.groupId === groupId
+            );
+
+            if (foundGroup) setGroup(() => foundGroup);
+        }
+    }, [groups, groupId]);
+
+    return group;
+}
