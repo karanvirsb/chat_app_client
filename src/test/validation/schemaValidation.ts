9636b89a55cd700c8fdeb5c4e4f397a75fca9dee
mrboparai <karanvirboparai123@gmail.com>
2022-11-21 12:15:50 -0500

    added check for undefined

--- src/test/validation/schemaValidation.ts
+++ src/test/validation/schemaValidation.ts
@@ -1,9 +1,13 @@
 import { IGroup } from "../../Redux/slices/groupApiSlice";
 
-export function isGroupArray(arr: IGroup[] | string): arr is IGroup[] {
+export function isGroupArray(
+    arr: IGroup[] | string | undefined
+): arr is IGroup[] {
+    if (arr === undefined) return false;
     return (arr as IGroup[]).map !== undefined;
 }
 
 export function isGroup(item: IGroup | string | undefined): item is IGroup {
+    if (item === undefined) return false;
     return (item as IGroup).groupId !== undefined;
 }
