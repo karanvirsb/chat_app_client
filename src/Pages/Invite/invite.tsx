f8d8ff9284292714e4cec931ed833d92357b08aa
mrboparai <karanvirboparai123@gmail.com>
2022-11-22 09:42:22 -0500

    created an invite page

---
+++ src/Pages/Invite/invite.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+import { useNavigate, useParams } from "react-router";
+
+import Spinner from "../../Components/Spinner/Spinner";
+import useGetSession from "../../Hooks/useGetSession";
+
+export default function invite() {
+    const { sessionInfo } = useGetSession();
+    const { inviteCode } = useParams();
+    // get the location from
+    const navigate = useNavigate();
+    // if user is logged in send the invite code to the modal
+    useEffect(() => {
+        if (sessionInfo?.doesSessionExist) {
+            navigate(`/home/${inviteCode}`); // either navigate home and send open modal for join group
+        }
+    }, [sessionInfo]);
+    // if user is not logged in log in and then send to invite code modal
+    return (
+        <div className='flex justify-center items-center h-screen w-full'>
+            <Spinner></Spinner>
+        </div>
+    );
+}
