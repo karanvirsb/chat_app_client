e1072ceee26da8bec3ce2cc7a0e43942612046d5
mrboparai <karanvirboparai123@gmail.com>
2022-11-22 11:34:40 -0500

    added dispatch

--- src/Pages/Invite/invite.tsx
+++ src/Pages/Invite/invite.tsx
@@ -2,17 +2,27 @@ import React, { useEffect } from "react";
 import { useNavigate, useParams } from "react-router";
 
 import Spinner from "../../Components/Spinner/Spinner";
+import { useAppDispatch } from "../../Hooks/reduxHooks";
 import useGetSession from "../../Hooks/useGetSession";
+import { setModal } from "../../Redux/slices/modalSlice";
 
 export default function Invite() {
     const { sessionInfo } = useGetSession();
     const { inviteCode } = useParams();
+    const dispatch = useAppDispatch();
     // get the location from
     const navigate = useNavigate();
     // if user is logged in send the invite code to the modal
     useEffect(() => {
         if (sessionInfo?.doesSessionExist) {
-            navigate(`/home/${inviteCode}`); // either navigate home and send open modal for join group
+            navigate(`/`); // either navigate home and send open modal for join group
+            dispatch(
+                setModal({
+                    open: true,
+                    modalName: "joinGroup",
+                    options: { inviteCode },
+                })
+            );
         }
     }, [sessionInfo]);
     // if user is not logged in log in and then send to invite code modal
