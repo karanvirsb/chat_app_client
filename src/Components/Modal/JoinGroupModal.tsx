67dbd7df78fb8e3a811b41be7103db819801b353
mrboparai <karanvirboparai123@gmail.com>
2022-11-22 11:34:30 -0500

    added error checking

--- src/Components/Modal/JoinGroupModal.tsx
+++ src/Components/Modal/JoinGroupModal.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useAppDispatch } from "../../Hooks/reduxHooks";
 import {
     useGetGroupByInviteCodeQuery,
     useGetGroupUsersQuery,
 } from "../../Redux/slices/groupApiSlice";
 import { resetModal } from "../../Redux/slices/modalSlice";
-import { isGroup } from "../../test/validation/schemaValidation";
+import { areGroupUsers, isGroup } from "../../test/validation/schemaValidation";
 import BtnCallToAction from "../Buttons/BtnCallToAction";
 import BtnCancelAction from "../Buttons/BtnCancelAction";
 import Modal from "./Modal";
@@ -18,12 +18,54 @@ type props = {
 // TODO get group and members
 // TODO display are you sure you want to join [groupName] and then how many members are online and offline
 export default function JoinGroupModal({ inviteCode }: props) {
+    const [skipGroupUsers, setSkipGroupUsers] = useState(true);
     const dispatch = useAppDispatch();
-    const { data: group } = useGetGroupByInviteCodeQuery(inviteCode);
+    const { data: group, isSuccess } = useGetGroupByInviteCodeQuery(inviteCode);
     const { data: groupUsers, isLoading: areUsersLoading } =
         useGetGroupUsersQuery(isGroup(group) ? group.groupId : "", {
-            skip: group ? false : true,
+            skip: skipGroupUsers,
         });
+
+    useEffect(() => {
+        if (isSuccess && isGroup(group)) {
+            setSkipGroupUsers(false);
+        }
+    }, [group]);
+
+    // if group is an error
+    if (!isGroup(group))
+        return (
+            <Modal modalName='Join A Group' modalClass='flex'>
+                <>
+                    <div>
+                        <p>Error: {group}</p>
+                    </div>
+                    <div className='flex gap-4 mt-2'>
+                        <BtnCancelAction
+                            text='Ok'
+                            onClick={closeModal}
+                        ></BtnCancelAction>
+                    </div>
+                </>
+            </Modal>
+        );
+
+    // if users are errored
+    if (!areGroupUsers(groupUsers)) {
+        <Modal modalName='Join A Group' modalClass='flex'>
+            <>
+                <div>
+                    <p>Error: {groupUsers}</p>
+                </div>
+                <div className='flex gap-4 mt-2'>
+                    <BtnCancelAction
+                        text='Ok'
+                        onClick={closeModal}
+                    ></BtnCancelAction>
+                </div>
+            </>
+        </Modal>;
+    }
 
     return (
         <Modal modalName='Join A Group' modalClass='flex'>
