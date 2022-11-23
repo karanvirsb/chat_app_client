7d3d097459c7cfdca08df798d994aa17b8aa21fb
mrboparai <karanvirboparai123@gmail.com>
2022-11-22 22:09:41 -0500

    renamed

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modalReducer from "./slices/modalSlice";
-import sideBarReducer from "./slices/sideBarSlice";
+import sideBarReducer from "./slices/SideBarSlice";
 import { groupApiSlice } from "./slices/groupApiSlice";
 
 export const store = configureStore({
