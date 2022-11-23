069b58aa288aebe43e7ea620e206387c10cfda49
mrboparai <karanvirboparai123@gmail.com>
2022-11-23 12:05:07 -0500

    changed to ffalse

--- src/Sockets/index.ts
+++ src/Sockets/index.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 const socket = io("http://localhost:8000", {
     reconnectionAttempts: 2,
-    closeOnBeforeunload: true,
+    closeOnBeforeunload: false,
     autoConnect: true,
 });
 
