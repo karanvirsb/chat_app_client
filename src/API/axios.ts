8c1e2d88728416716171f0ed54a96e2b1bc76417
mrboparai <karanvirboparai123@gmail.com>
2022-11-24 10:20:34 -0500

    added base url

--- src/API/axios.ts
+++ src/API/axios.ts
@@ -1,3 +1,5 @@
 import axios from "axios";
 import Session from "supertokens-auth-react/recipe/session";
 Session.addAxiosInterceptors(axios);
+axios.create({ baseURL: "http://localhost:8000" });
+export default axios;
