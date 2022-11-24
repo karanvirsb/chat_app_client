6349227d3785ae0b87a8f2e7f9bb3da3b3397d0d
mrboparai <karanvirboparai123@gmail.com>
2022-11-24 10:07:35 -0500

    added query client provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,18 @@ import { Provider } from "react-redux";
 import { store } from "./Redux/store";
 import App from "./App";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <Router>
-            <Provider store={store}>
-                <App />
-            </Provider>
+            <QueryClientProvider client={queryClient}>
+                <Provider store={store}>
+                    <App />
+                </Provider>
+            </QueryClientProvider>
         </Router>
     </React.StrictMode>
 );
