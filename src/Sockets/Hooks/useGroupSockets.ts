3fc69c8751dc233bd4bd2cc89f0ce70af4219447
mrboparai <karanvirboparai123@gmail.com>
2022-11-26 09:56:53 -0500

    added delete group and join group

--- src/Sockets/Hooks/useGroupSockets.ts
+++ src/Sockets/Hooks/useGroupSockets.ts
@@ -2,7 +2,10 @@ import socket from "..";
 import { socketEvent } from "../SocketHandler";
 
 export default function useGroupSockets() {
-    return (event: "updated_group_name", data: socketEvent) => {
+    return (
+        event: "updated_group_name" | "delete_the_group" | "join_group",
+        data: socketEvent
+    ) => {
         socket.emit(event, data);
     };
 }
