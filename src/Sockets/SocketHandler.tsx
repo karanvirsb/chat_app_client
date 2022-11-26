8410f469d8622773eb937b54d960895bab59eb3f
mrboparai <karanvirboparai123@gmail.com>
2022-11-26 09:56:41 -0500

    added delete group event

--- src/Sockets/SocketHandler.tsx
+++ src/Sockets/SocketHandler.tsx
@@ -1,5 +1,5 @@
 import { useQueryClient } from "@tanstack/react-query";
-import React, { ReactElement, useEffect } from "react";
+import React, { useEffect } from "react";
 import socket from ".";
 import { IGroup } from "../Hooks/groupHooks";
 
@@ -17,13 +17,17 @@ export type UpdateEvent = {
     payload: Partial<IGroup>;
 };
 
+export type DeleteEvent = {
+    groupId: string;
+    payload: {};
+};
+
 export type socketEvent = InvalidateEvent | UpdateEvent;
 
 export default function SocketHandler({ children }: props) {
     const queryClient = useQueryClient();
     useEffect(() => {
         socket.on("update_group_name", (data: UpdateEvent) => {
-            console.log("water");
             queryClient.setQueriesData(["groups"], (oldData: unknown) => {
                 const update = (entity: IGroup) =>
                     entity.groupId === data.groupId
@@ -32,8 +36,19 @@ export default function SocketHandler({ children }: props) {
                 return Array.isArray(oldData) && oldData.map(update);
             });
         });
+
+        socket.on("delete_group", (data: DeleteEvent) => {
+            console.log(data);
+            queryClient.setQueriesData(["groups"], (oldData: unknown) => {
+                const deleteGroup = (group: IGroup) =>
+                    group.groupId !== data.groupId;
+                return Array.isArray(oldData) && oldData.filter(deleteGroup);
+            });
+        });
+
         return () => {
             socket.off("update_group_name"); // clean up
+            socket.off("delete_group");
         };
     }, [queryClient]);
     return children;
