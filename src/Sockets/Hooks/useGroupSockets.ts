dc4174c2e75c8fb142639f82601bbf527b534b5f
mrboparai <karanvirboparai123@gmail.com>
2022-11-26 20:29:35 -0500

    updated to rooms

--- src/Sockets/Hooks/useGroupSockets.ts
+++ src/Sockets/Hooks/useGroupSockets.ts
@@ -3,7 +3,7 @@ import { socketEvent } from "../SocketHandler";
 
 export default function useGroupSockets() {
     return (
-        event: "updated_group_name" | "delete_the_group" | "join_group",
+        event: "updated_group_name" | "delete_the_group" | "join_rooms",
         data: socketEvent
     ) => {
         socket.emit(event, data);
