9531afeca8159b2dcb0725cda643ad445a209c0d
mrboparai <karanvirboparai123@gmail.com>
2022-11-28 09:27:36 -0500

    added event type

--- src/Sockets/Hooks/useGroupSockets.ts
+++ src/Sockets/Hooks/useGroupSockets.ts
@@ -3,7 +3,11 @@ import { socketEvent } from "../SocketHandler";
 
 export default function useGroupSockets() {
     return (
-        event: "updated_group_name" | "delete_the_group" | "join_rooms",
+        event:
+            | "updated_group_name"
+            | "delete_the_group"
+            | "join_rooms"
+            | "update_the_group_users",
         data: socketEvent
     ) => {
         socket.emit(event, data);
