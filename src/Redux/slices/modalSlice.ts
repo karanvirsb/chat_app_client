a2460a530940b7eca5307f24e14d9215df4b2c3c
karanvirsb <karanvirboparai123@gmail.com>
2023-01-01 12:00:19 -0500

    added new modal

--- src/Redux/slices/modalSlice.ts
+++ src/Redux/slices/modalSlice.ts
@@ -4,47 +4,48 @@ import type { PayloadAction } from "@reduxjs/toolkit";
 
 // Define a type for the slice state
 interface ModalState {
-    open: boolean;
-    options: any;
-    modalName:
-        | "changeGroupName"
-        | "inviteUser"
-        | "deleteGroup"
-        | "leaveGroup"
-        | "createGroup"
-        | "addFriend"
-        | "deleteAccount"
-        | "editUsername"
-        | "editEmail"
-        | "editPassword"
-        | "joinGroup"
-        | "";
+  open: boolean;
+  options: any;
+  modalName:
+    | "changeGroupName"
+    | "inviteUser"
+    | "deleteGroup"
+    | "leaveGroup"
+    | "createGroup"
+    | "addFriend"
+    | "deleteAccount"
+    | "editUsername"
+    | "editEmail"
+    | "editPassword"
+    | "joinGroup"
+    | "createGroupChannel"
+    | "";
 }
 
 // Define the initial state using that type
 const initialState: ModalState = {
-    modalName: "",
-    options: {},
-    open: false,
+  modalName: "",
+  options: {},
+  open: false,
 };
 
 export const modalSlice = createSlice({
-    name: "modal",
-    // `createSlice` will infer the state type from the `initialState` argument
-    initialState,
-    reducers: {
-        setModal: (state, action: PayloadAction<ModalState>) => {
-            const { modalName, options, open } = action.payload;
-            state.modalName = modalName;
-            state.open = open;
-            state.options = options;
-        },
-        resetModal: (state) => {
-            state.modalName = "";
-            state.open = false;
-            state.options = {};
-        },
+  name: "modal",
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    setModal: (state, action: PayloadAction<ModalState>) => {
+      const { modalName, options, open } = action.payload;
+      state.modalName = modalName;
+      state.open = open;
+      state.options = options;
     },
+    resetModal: (state) => {
+      state.modalName = "";
+      state.open = false;
+      state.options = {};
+    },
+  },
 });
 
 export const { setModal, resetModal } = modalSlice.actions;
