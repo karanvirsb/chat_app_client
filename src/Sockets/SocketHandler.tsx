da5cf8cf2301699597522c457803ae9d741e14ca
karanvirsb <karanvirboparai123@gmail.com>
2023-01-01 11:22:21 -0500

    added import types

--- src/Sockets/SocketHandler.tsx
+++ src/Sockets/SocketHandler.tsx
@@ -2,103 +2,78 @@ import { useQueryClient } from "@tanstack/react-query";
 import React, { useEffect } from "react";
 import socket from ".";
 import { IGroup, IUser } from "../Hooks/groupHooks";
+import {
+  InvalidateEvent,
+  UpdateEvent,
+  JoinRoomEvent,
+  UpdateGroupUsersEvent,
+  LeaveRoomEvent,
+  LeaveGroupEvent,
+  DeleteEvent,
+} from "./types/groupTypes";
 
 type props = {
-    children: JSX.Element;
+  children: JSX.Element;
 };
 
-export type InvalidateEvent = {
-    queryTags: string[];
-    groupId: string;
-};
+type groupEvents =
+  | InvalidateEvent
+  | UpdateEvent
+  | JoinRoomEvent
+  | UpdateGroupUsersEvent
+  | LeaveRoomEvent
+  | LeaveGroupEvent;
 
-export type UpdateEvent = {
-    groupId: string;
-    payload: Partial<IGroup>;
-};
-
-export type DeleteEvent = {
-    groupId: string;
-    payload: {};
-};
-
-export type JoinRoomEvent = {
-    rooms: string[];
-    userId: string;
-};
-
-export type UpdateGroupUsersEvent = {
-    groupId: string;
-    payload: { userInfo: IUser };
-};
-
-export type LeaveRoomEvent = {
-    groupId: string;
-    payload: { userId: string };
-};
-
-export type LeaveGroupEvent = {
-    groupId: string;
-    payload: { userId: string };
-};
-
-export type socketEvent =
-    | InvalidateEvent
-    | UpdateEvent
-    | JoinRoomEvent
-    | UpdateGroupUsersEvent
-    | LeaveRoomEvent
-    | LeaveGroupEvent;
+export type socketEvent = groupEvents;
 
 export default function SocketHandler({ children }: props) {
-    const queryClient = useQueryClient();
-    useEffect(() => {
-        socket.on("update_group_name", (data: UpdateEvent) => {
-            queryClient.setQueriesData(["groups"], (oldData: unknown) => {
-                const update = (entity: IGroup) =>
-                    entity.groupId === data.groupId
-                        ? { ...entity, ...data.payload }
-                        : entity;
-                return Array.isArray(oldData) && oldData.map(update);
-            });
-        });
+  const queryClient = useQueryClient();
+  useEffect(() => {
+    socket.on("update_group_name", (data: UpdateEvent) => {
+      queryClient.setQueriesData(["groups"], (oldData: unknown) => {
+        const update = (entity: IGroup) =>
+          entity.groupId === data.groupId
+            ? { ...entity, ...data.payload }
+            : entity;
+        return Array.isArray(oldData) && oldData.map(update);
+      });
+    });
 
-        socket.on("delete_group", (data: DeleteEvent) => {
-            queryClient.setQueriesData(["groups"], (oldData: unknown) => {
-                const deleteGroup = (group: IGroup) =>
-                    group.groupId !== data.groupId;
-                return Array.isArray(oldData) && oldData.filter(deleteGroup);
-            });
-        });
+    socket.on("delete_group", (data: DeleteEvent) => {
+      queryClient.setQueriesData(["groups"], (oldData: unknown) => {
+        const deleteGroup = (group: IGroup) => group.groupId !== data.groupId;
+        return Array.isArray(oldData) && oldData.filter(deleteGroup);
+      });
+    });
 
-        socket.on("update_group_users", (data: UpdateGroupUsersEvent) => {
-            queryClient.setQueriesData(
-                [`group-users-${data.groupId}`],
-                (oldData: unknown) => {
-                    const pushResult = (arr: IUser[]) => {
-                        return [...arr, data.payload.userInfo];
-                    };
-                    return Array.isArray(oldData) && pushResult(oldData);
-                }
-            );
-        });
+    socket.on("update_group_users", (data: UpdateGroupUsersEvent) => {
+      queryClient.setQueriesData(
+        [`group-users-${data.groupId}`],
+        (oldData: unknown) => {
+          const pushResult = (arr: IUser[]) => {
+            return [...arr, data.payload.userInfo];
+          };
+          return Array.isArray(oldData) && pushResult(oldData);
+        }
+      );
+    });
 
-        socket.on("removed_user", (data: LeaveGroupEvent) => {
-            queryClient.setQueriesData(
-                [`group-users-${data.groupId}`],
-                (oldData: unknown) => {
-                    const removeUser = (user: IUser) =>
-                        user.userId !== data.payload.userId;
-                    return Array.isArray(oldData) && oldData.filter(removeUser);
-                }
-            );
-        });
+    socket.on("removed_user", (data: LeaveGroupEvent) => {
+      queryClient.setQueriesData(
+        [`group-users-${data.groupId}`],
+        (oldData: unknown) => {
+          const removeUser = (user: IUser) =>
+            user.userId !== data.payload.userId;
+          return Array.isArray(oldData) && oldData.filter(removeUser);
+        }
+      );
+    });
 
-        return () => {
-            socket.off("update_group_name"); // clean up
-            socket.off("delete_group");
-            socket.off("update_group_users");
-        };
-    }, [queryClient]);
-    return children;
+    return () => {
+      socket.off("update_group_name"); // clean up
+      socket.off("delete_group");
+      socket.off("update_group_users");
+    };
+  }, [queryClient]);
+  return children;
 }
