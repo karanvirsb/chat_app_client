8fc4e86dd12c20c009e3eaae0aa5f7432720537c
karanvirsb <karanvirboparai123@gmail.com>
2023-01-01 11:21:56 -0500

    created types

---
+++ src/Sockets/types/groupTypes.d.ts
@@ -0,0 +1,34 @@
+export type InvalidateEvent = {
+  queryTags: string[];
+  groupId: string;
+};
+
+export type UpdateEvent = {
+  groupId: string;
+  payload: Partial<IGroup>;
+};
+
+export type DeleteEvent = {
+  groupId: string;
+  payload: {};
+};
+
+export type JoinRoomEvent = {
+  rooms: string[];
+  userId: string;
+};
+
+export type UpdateGroupUsersEvent = {
+  groupId: string;
+  payload: { userInfo: IUser };
+};
+
+export type LeaveRoomEvent = {
+  groupId: string;
+  payload: { userId: string };
+};
+
+export type LeaveGroupEvent = {
+  groupId: string;
+  payload: { userId: string };
+};
