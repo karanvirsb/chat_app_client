602798222f622a2dea7bcb3f5e25f6e1b7c44afc
karanvirsb <karanvirboparai123@gmail.com>
2023-01-04 17:21:48 -0500

    added fetch into chat messages

--- src/Pages/Home/Components/GroupChat.tsx
+++ src/Pages/Home/Components/GroupChat.tsx
@@ -1,22 +1,33 @@
 import React from "react";
+import { useGetGroupMessagesByChannelIdQuery } from "../../../Hooks/groupChatHooks";
 
 type props = {
-    channelId: string;
+  channelId: string;
 };
 
 export default function GroupChat({ channelId }: props) {
-    return (
-        <div className='bg-chat-bg flex flex-col flex-grow h-full'>
-            <div className='flex-grow p-4 w-full '>
-                {/* TODO Create chat component */}
-            </div>
-            <div className='input-group p-4'>
-                <input
-                    type='text'
-                    placeholder='Send a message'
-                    className='input input-bordered bg-[#2a303c] w-full focus:outline-none'
-                />
-            </div>
-        </div>
-    );
+  const {
+    data: chatMessages,
+    isFetching,
+    isLoading,
+    isError,
+  } = useGetGroupMessagesByChannelIdQuery({
+    channelId,
+    dateCreated: new Date(),
+    limit: 10,
+  });
+  return (
+    <div className="bg-chat-bg flex flex-col flex-grow h-full">
+      <div className="flex-grow p-4 w-full ">
+        {/* TODO Create chat component */}
+      </div>
+      <div className="input-group p-4">
+        <input
+          type="text"
+          placeholder="Send a message"
+          className="input input-bordered bg-[#2a303c] w-full focus:outline-none"
+        />
+      </div>
+    </div>
+  );
 }
