258b165b3f63704f4ed2259e7f85ec3a7df88462
karanvirsb <karanvirboparai123@gmail.com>
2023-01-04 17:17:44 -0500

    created group messages hooks

---
+++ src/Hooks/groupChatHooks.ts
@@ -0,0 +1,48 @@
+import axios from "../API/axios";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+
+export interface IMessage {
+  userId: string;
+  dateCreated: Date;
+  messageId: string;
+  dateModified?: Date;
+  replyTo?: string;
+  text: string;
+  channelId: string;
+}
+
+interface ReturnGroupChannels {
+  success: boolean;
+  data: IMessage[] | undefined;
+  error: string;
+}
+
+// setting up global variables
+const baseurl = "http://localhost:8000/groupMessage";
+
+function useGetGroupMessagesByChannelIdQuery({
+  channelId,
+  dateCreated,
+  limit,
+}: {
+  channelId: string;
+  dateCreated: Date;
+  limit: number;
+}) {
+  const getMessages = async (): Promise<IMessage[] | undefined> => {
+    const resp = await axios({
+      url: `${baseurl}/channel`,
+      params: { channelId, dateCreated, limit },
+      method: "GET",
+    });
+    const data: ReturnGroupChannels = resp.data;
+    return data.data;
+  };
+
+  return useQuery({
+    queryKey: [`group-messages-${channelId}`],
+    queryFn: getMessages,
+  });
+}
+
+export { useGetGroupMessagesByChannelIdQuery };
