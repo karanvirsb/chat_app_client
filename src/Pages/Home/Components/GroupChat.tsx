2bd02fa5c57da14d752029bf0d56e0ce441002a6
karanvirsb <karanvirboparai123@gmail.com>
2023-01-10 11:39:16 -0500

    displayed messages along with added an id

--- src/Pages/Home/Components/GroupChat.tsx
+++ src/Pages/Home/Components/GroupChat.tsx
@@ -4,12 +4,14 @@ import {
   useGetGroupMessagesByChannelIdQuery,
 } from "../../../Hooks/groupChatHooks";
 import useGetSession from "../../../Hooks/useGetSession";
+import Messages from "../../../Components/Messages/Messages";
 
 type props = {
   channelId: string;
+  groupId: string;
 };
 
-export default function GroupChat({ channelId }: props) {
+export default function GroupChat({ channelId, groupId }: props) {
   const messageRef = useRef<null | HTMLInputElement>(null);
   // TODO after inital load need to set dateCreated to last message.
   const {
@@ -32,15 +34,16 @@ export default function GroupChat({ channelId }: props) {
     <div className="bg-chat-bg flex flex-col flex-grow h-full">
       <div className="flex-grow p-4 w-full ">
         {/* TODO Create chat component */}
-        {chatMessages &&
-          chatMessages.pages.map((page) => {
-            return (
-              <div>
-                <p>{page && JSON.stringify(page.nextPage)}</p>
-                <div>{JSON.stringify(page?.data)}</div>
-              </div>
-            );
-          })}
+        {chatMessages
+          ? chatMessages?.pages.map((messages) => {
+              return (
+                <Messages
+                  groupId={groupId}
+                  messages={messages?.data}
+                ></Messages>
+              );
+            })
+          : null}
         <button onClick={() => fetchNextPage()}>fetch next messages</button>
       </div>
 
