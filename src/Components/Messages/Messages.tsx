179d229b9d4b5cfa0ed95c4d22ab1c80d1445ab0
karanvirsb <karanvirboparai123@gmail.com>
2023-01-10 13:15:59 -0500

    removed logs

--- src/Components/Messages/Messages.tsx
+++ src/Components/Messages/Messages.tsx
@@ -16,14 +16,14 @@ export default function Messages({ messages, groupId }: props) {
   const groupUsers = queryClient.getQueriesData([
     `group-users-${groupId}`,
   ])[0][1] as unknown[];
-  console.log(groupUsers);
+
   return (
     <>
       {messages?.map((message) => {
         const foundUser = isGroupUsers(groupUsers)
           ? groupUsers.find((user) => user.userId === message.userId)
           : undefined;
-        console.log(foundUser);
+
         return (
           <div
             key={message.messageId}
