54bab9164c2ba45a556eec54c92fc8b80cc4c3b3
karanvirsb <karanvirboparai123@gmail.com>
2023-01-13 10:19:46 -0500

    removed local storage

--- src/Pages/Home/home.tsx
+++ src/Pages/Home/home.tsx
@@ -1,23 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "./Components/Sidebar";
 import GroupChannel from "./Components/GroupChannel";
 import MeChannel from "./Components/MeChannel";
 import useLocalStorage from "../../Hooks/useLocalStorage";
 
 export default function Home() {
-    // const [tab, setTab] = useState<"group" | "me">("me");
-    const [tab, setTab] = useLocalStorage<"group" | "me">("groupOrMeTab", "me");
-    const [tabId, setTabId] = useLocalStorage<string>("tabId", "-1"); // can be a userId or groupId
+  const [tab, setTab] = useState<"group" | "me">("me");
+  // const [tab, setTab] = useLocalStorage<"group" | "me">("groupOrMeTab", "me");
+  const [tabId, setTabId] = useState("-1");
+  // const [tabId, setTabId] = useLocalStorage<string>("tabId", "-1"); // can be a userId or groupId
 
-    return (
-        <>
-            <div className='flex h-screen'>
-                <Sidebar setTab={setTab} setTabId={setTabId}></Sidebar>
-                {tab === "group" && (
-                    <GroupChannel groupId={tabId}></GroupChannel>
-                )}
-                {tab === "me" && <MeChannel></MeChannel>}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="flex h-screen max-h-screen min-h-screen">
+        <Sidebar setTab={setTab} setTabId={setTabId}></Sidebar>
+        {tab === "group" && <GroupChannel groupId={tabId}></GroupChannel>}
+        {tab === "me" && <MeChannel></MeChannel>}
+      </div>
+    </>
+  );
 }
