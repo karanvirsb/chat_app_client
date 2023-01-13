59a4477ab266accc99cd609704de474063d687e6
karanvirsb <karanvirboparai123@gmail.com>
2023-01-13 11:35:25 -0500

    made height full screen

--- src/Components/ChannelContainer/ChannelContainer.tsx
+++ src/Components/ChannelContainer/ChannelContainer.tsx
@@ -9,7 +9,7 @@ export default function ChannelContainer({ children }: props) {
   const isSideBarOpen = useAppSelector((state) => state.sideBarReducer.open);
   return (
     <div
-      className={`flex flex-col w-full h-[75vh] ${
+      className={`flex flex-col w-full h-full ${
         isSideBarOpen ? "md:translate-x-[20.125rem]" : "md:translate-x-[0px]"
       }`}
     >
