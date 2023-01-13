cdb39541455aa1a6e662c12ddf3542d1d1f828d5
karanvirsb <karanvirboparai123@gmail.com>
2023-01-13 11:21:08 -0500

    added height

--- src/Components/ChannelContainer/ChannelContainer.tsx
+++ src/Components/ChannelContainer/ChannelContainer.tsx
@@ -2,20 +2,18 @@ import React from "react";
 import { useAppSelector } from "../../Hooks/reduxHooks";
 
 type props = {
-    children: JSX.Element;
+  children: JSX.Element;
 };
 
 export default function ChannelContainer({ children }: props) {
-    const isSideBarOpen = useAppSelector((state) => state.sideBarReducer.open);
-    return (
-        <div
-            className={`flex flex-col flex-grow ${
-                isSideBarOpen
-                    ? "md:translate-x-[20.125rem]"
-                    : "md:translate-x-[0px]"
-            }`}
-        >
-            {children}
-        </div>
-    );
+  const isSideBarOpen = useAppSelector((state) => state.sideBarReducer.open);
+  return (
+    <div
+      className={`flex flex-col w-full h-[75vh] ${
+        isSideBarOpen ? "md:translate-x-[20.125rem]" : "md:translate-x-[0px]"
+      }`}
+    >
+      {children}
+    </div>
+  );
 }
