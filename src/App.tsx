68ca3eb05b265ab1f4780bf1431538ab9878d838
karanvirsb <karanvirboparai123@gmail.com>
2023-01-16 09:24:43 -0500

    adjusted format

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,63 @@
-import React from "react";
-import { Routes, Route, Outlet } from "react-router-dom";
-import * as reactRouterDom from "react-router-dom";
-import Home from "./Pages/Home/home";
-import "./App.css";
+import React from 'react'
+import { Routes, Route, Outlet } from 'react-router-dom'
+import * as reactRouterDom from 'react-router-dom'
+import Home from './Pages/Home/home'
+import './App.css'
 import SuperTokens, {
-    getSuperTokensRoutesForReactRouterDom,
-    SuperTokensWrapper,
-} from "supertokens-auth-react";
+  getSuperTokensRoutesForReactRouterDom,
+  SuperTokensWrapper
+} from 'supertokens-auth-react'
 import EmailPassword, {
-    EmailPasswordAuth,
-} from "supertokens-auth-react/recipe/emailpassword";
-import Session from "supertokens-auth-react/recipe/session";
-import ModalDisplay from "./Components/Modal/ModalDisplay";
-import Settings from "./Pages/Settings/settings";
-import socket from "./Sockets";
-import BottomBar from "./Components/BottomBar/BottomBar";
-import Friends from "./Pages/Friends/friends";
-import Search from "./Pages/Search/search";
-import Invite from "./Pages/Invite/invite";
+  EmailPasswordAuth
+} from 'supertokens-auth-react/recipe/emailpassword'
+import Session from 'supertokens-auth-react/recipe/session'
+import ModalDisplay from './Components/Modal/ModalDisplay'
+import Settings from './Pages/Settings/settings'
+import socket from './Sockets'
+import BottomBar from './Components/BottomBar/BottomBar'
+import Friends from './Pages/Friends/friends'
+import Search from './Pages/Search/search'
+import Invite from './Pages/Invite/invite'
 
 SuperTokens.init({
-    appInfo: {
-        // learn more about this on https://supertokens.com/docs/emailpassword/appinfo
-        appName: "chat app",
-        apiDomain: "http://localhost:8000",
-        websiteDomain: "http://localhost:3000",
-        apiBasePath: "/auth",
-        websiteBasePath: "/auth",
-    },
-    recipeList: [
-        EmailPassword.init({
-            signInAndUpFeature: {
-                signUpForm: {
-                    formFields: [
-                        {
-                            id: "username",
-                            label: "Username",
-                            placeholder: "Enter an username",
-                        },
-                        {
-                            id: "email",
-                            label: "Email",
-                            placeholder: "Enter an email",
-                        },
-                        {
-                            id: "password",
-                            label: "password",
-                            placeholder: "Enter a password",
-                        },
-                    ],
-                },
+  appInfo: {
+    // learn more about this on https://supertokens.com/docs/emailpassword/appinfo
+    appName: 'chat app',
+    apiDomain: 'http://localhost:8000',
+    websiteDomain: 'http://localhost:3000',
+    apiBasePath: '/auth',
+    websiteBasePath: '/auth'
+  },
+  recipeList: [
+    EmailPassword.init({
+      signInAndUpFeature: {
+        signUpForm: {
+          formFields: [
+            {
+              id: 'username',
+              label: 'Username',
+              placeholder: 'Enter an username'
             },
-        }),
-        Session.init(),
-    ],
-});
+            {
+              id: 'email',
+              label: 'Email',
+              placeholder: 'Enter an email'
+            },
+            {
+              id: 'password',
+              label: 'password',
+              placeholder: 'Enter a password'
+            }
+          ]
+        }
+      }
+    }),
+    Session.init()
+  ]
+})
 
-export default function App() {
-    return (
+export default function App () {
+  return (
         <>
             <SuperTokensWrapper>
                 <ModalDisplay></ModalDisplay>
@@ -108,14 +108,14 @@ export default function App() {
                 </Routes>
             </SuperTokensWrapper>
         </>
-    );
+  )
 
-    function MobileLayout() {
-        return (
-            <>
-                <Outlet></Outlet>
-                <BottomBar></BottomBar>
-            </>
-        );
-    }
+  function MobileLayout () {
+    return (
+      <>
+          <Outlet></Outlet>
+          <BottomBar></BottomBar>
+      </>
+    )
+  }
 }
