3bc214ee79c623bd1bb5a0832af7f999a0de661a
karanvirsb <karanvirboparai123@gmail.com>
2023-01-16 10:06:26 -0500

    added type to ref

--- src/Hooks/useComponentVisible.ts
+++ src/Hooks/useComponentVisible.ts
@@ -1,22 +1,20 @@
-import { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 /**
  * Hook is used to check the visibility of an element usually a pop up menu
  * @returns Object {ref: the reference to the pop up element, isComponentVisible: whether the ref is visible, setIsComponentVisible: setting visibility of element}
  */
 
 interface IUseComponentVisible {
-    
-    ref: any;
+    ref: React.RefObject<any>;
     isComponentVisible: boolean;
     setIsComponentVisible: React.Dispatch<React.SetStateAction<boolean>>;
-
 }
 
 export default function useComponentVisible(): IUseComponentVisible {
     const [isComponentVisible, setIsComponentVisible] = useState(false);
-    const ref: any = useRef(null);
+    const ref: React.RefObject<any> = useRef(null);
 
-    const handleClickOutside = (event: any) => {
+    const handleClickOutside = (event: any): void => {
         if (ref.current && !ref.current.contains(event.target)) {
             setIsComponentVisible(false);
         }
