9d1b505a1da01cfbf1a6fbdc7129d110602412b7
karanvirsb <karanvirboparai123@gmail.com>
2023-01-16 09:42:28 -0500

    added return type

--- src/utilities/toCapitalize.ts
+++ src/utilities/toCapitalize.ts
@@ -1,4 +1,4 @@
-export default function toCapitalize(str: string | undefined) {
+export default function toCapitalize(str: string | undefined): string {
   if (str === undefined) return "";
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
