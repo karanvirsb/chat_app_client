badb8282edf70126af12c3226268e6792a9deee8
karanvirsb <karanvirboparai123@gmail.com>
2023-01-23 10:43:48 -0500

    removed unused

--- src/Pages/Home/Components/GroupChat.tsx
+++ src/Pages/Home/Components/GroupChat.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import {
   useCreateGroupMessageMutation,
-  useDeleteGroupMessageMutation,
   useEditMessageTextMutation,
   useGetGroupMessagesByChannelIdQuery,
 } from "../../../Hooks/groupChatHooks";
