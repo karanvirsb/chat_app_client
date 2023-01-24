f803970463ed81d9214e6f7540da8b600fa74e53
karanvirsb <karanvirboparai123@gmail.com>
2023-01-24 08:57:08 -0500

    added svg and removed extra html tag

--- src/Components/Collapse/Collapse.tsx
+++ src/Components/Collapse/Collapse.tsx
@@ -11,66 +11,73 @@ const Collapse = ({ children, title, clickEvent }: props) => {
   return (
     // TODO add transition time
     <>
-      <div className="flex flex-col items-start px-2">
-        <p className="grid grid-cols-[1fr_3fr_1fr] items-center gap-2 w-full py-4">
-          <span
-            className="text-md flex items-center justify-self-end self-start"
+      <div className="grid grid-cols-[1fr_3fr_1fr] items-center gap-2 w-full py-4 px-2">
+        <span
+          className="text-md flex items-center justify-self-end self-start"
+          onClick={() => {
+            setClicked((prev) => !prev);
+          }}
+        >
+          {clicked ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-5 h-6 opacity-90"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-5 h-6 opacity-90"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8.25 4.5l7.5 7.5-7.5 7.5"
+              />
+            </svg>
+          )}
+        </span>
+        <div className="justify-self-start self-start flex flex-col">
+          <p
+            className="tracking-wide"
             onClick={() => {
               setClicked((prev) => !prev);
             }}
           >
-            {clicked ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-4 h-4"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-4 h-4"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M8.25 4.5l7.5 7.5-7.5 7.5"
-                />
-              </svg>
-            )}
-          </span>
-          <div className="justify-self-start self-start flex flex-col">
-            <p
-              onClick={() => {
-                setClicked((prev) => !prev);
-              }}
-            >
-              {title}
-            </p>
-            {clicked && children}
-          </div>
-          {/* TODO hover color */}
-          {clickEvent != null ? (
-            <span
-              className="text-2xl font-light cursor-pointer justify-self-center self-start"
-              onClick={clickEvent}
-            >
-              +
-            </span>
-          ) : null}
-        </p>
+            {title}
+          </p>
+          {clicked && children}
+        </div>
+        {/* TODO hover color */}
+        {clickEvent != null ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-5 h-6 justify-self-center self-start opacity-90"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 4.5v15m7.5-7.5h-15"
+            />
+          </svg>
+        ) : null}
       </div>
     </>
   );
