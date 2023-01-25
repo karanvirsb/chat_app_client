7732865147a5d0f9c45ee9692d467478a39f6819
karanvirsb <karanvirboparai123@gmail.com>
2023-01-25 09:26:47 -0500

    added event for changing user status to online

--- src/Pages/Home/Components/GroupList.tsx
+++ src/Pages/Home/Components/GroupList.tsx
@@ -3,81 +3,86 @@ import useGetSession from "../../../Hooks/useGetSession";
 import { useGetGroupsQuery } from "../../../Hooks/groupHooks";
 import socket from "../../../Sockets";
 import { isGroupArray } from "../../../test/validation/schemaValidation";
+import useLoginAndLogoutSockets from "../../../Sockets/Hooks/useLoginAndLogoutSockets";
 
 type props = {
-    activeIndex: number;
-    setTabToGroup: (id: string, index: number) => void;
+  activeIndex: number;
+  setTabToGroup: (id: string, index: number) => void;
 };
 
 export default function GroupList({ setTabToGroup, activeIndex }: props) {
-    const { sessionInfo } = useGetSession();
-    const {
-        data: groups,
-        isLoading,
-        isSuccess,
-    } = useGetGroupsQuery({ userId: sessionInfo?.userId });
+  const { sessionInfo } = useGetSession();
+  const {
+    data: groups,
+    isLoading,
+    isSuccess,
+  } = useGetGroupsQuery({ userId: sessionInfo?.userId });
+  const send = useLoginAndLogoutSockets();
+  useEffect(() => {
+    // check if its done loading and is successful then add groups into array and add rooms;
+    if (!isLoading && isSuccess && sessionInfo && isGroupArray(groups)) {
+      const groupIds = [];
 
-    useEffect(() => {
-        // check if its done loading and is successful then add groups into array and add rooms;
-        if (!isLoading && isSuccess && isGroupArray(groups)) {
-            const groupIds = [];
+      for (const group of groups) {
+        groupIds.push(group.groupId);
+      }
 
-            for (const group of groups) {
-                groupIds.push(group.groupId);
-            }
-
-            socket.emit("join_rooms", {
-                rooms: groupIds,
-                userId: sessionInfo?.userId,
-            });
-        }
-    }, [isLoading, isSuccess]);
-
-    let content;
-
-    if (isLoading) {
-        content = <>Loading...</>;
-    } else if (isSuccess) {
-        if (groups === undefined) {
-            content = <></>;
-        } else if (isGroupArray(groups)) {
-            content = groups.map((group, index) => {
-                if (index === activeIndex) {
-                    return (
-                        <li key={group.groupId}>
-                            <button
-                                className='btn btn-circle bg-white text-black'
-                                onClick={() => {
-                                    setTabToGroup(group.groupId, index);
-                                }}
-                            >
-                                {
-                                    group.groupName[0] // TODO split and get first index
-                                }
-                            </button>
-                        </li>
-                    );
-                } else {
-                    return (
-                        <li key={group.groupId}>
-                            <button
-                                className='btn btn-circle'
-                                onClick={() => {
-                                    setTabToGroup(group.groupId, index);
-                                }}
-                            >
-                                {
-                                    group.groupName[0] // TODO split and get first index
-                                }
-                            </button>
-                        </li>
-                    );
-                }
-            });
-        }
-    } else {
-        content = <button className='btn btn-circle'>!</button>;
-        console.error(groups);
+      socket.emit("join_rooms", {
+        rooms: groupIds,
+        userId: sessionInfo.userId,
+      });
+      send({
+        event: "login_user",
+        data: { userId: sessionInfo?.userId, payload: { groupIds: groupIds } },
+      });
     }
-    return <>{content}</>;
+  }, [isLoading, isSuccess]);
+
+  let content;
+
+  if (isLoading) {
+    content = <>Loading...</>;
+  } else if (isSuccess) {
+    if (groups === undefined) {
+      content = <></>;
+    } else if (isGroupArray(groups)) {
+      content = groups.map((group, index) => {
+        if (index === activeIndex) {
+          return (
+            <li key={group.groupId}>
+              <button
+                className="btn btn-circle bg-white text-black"
+                onClick={() => {
+                  setTabToGroup(group.groupId, index);
+                }}
+              >
+                {
+                  group.groupName[0] // TODO split and get first index
+                }
+              </button>
+            </li>
+          );
+        } else {
+          return (
+            <li key={group.groupId}>
+              <button
+                className="btn btn-circle"
+                onClick={() => {
+                  setTabToGroup(group.groupId, index);
+                }}
+              >
+                {
+                  group.groupName[0] // TODO split and get first index
+                }
+              </button>
+            </li>
+          );
+        }
+      });
+    }
+  } else {
+    content = <button className="btn btn-circle">!</button>;
+    console.error(groups);
+  }
+  return <>{content}</>;
 }
