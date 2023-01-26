cf8081bf0914657b49a110edc003d0c21c618c12
karanvirsb <karanvirboparai123@gmail.com>
2023-01-26 09:30:41 -0500

    prettier

--- src/Sockets/index.ts
+++ src/Sockets/index.ts
@@ -1,8 +1,8 @@
 import { io } from "socket.io-client";
 const socket = io("http://localhost:8000", {
-    reconnectionAttempts: 2,
-    closeOnBeforeunload: false,
-    autoConnect: true,
+  reconnectionAttempts: 2,
+  closeOnBeforeunload: false,
+  autoConnect: true,
 });
 
 export default socket;
